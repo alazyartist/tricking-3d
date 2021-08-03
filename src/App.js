b85cce3ae55aa4e1746955cd9daed1971050296d
Dylan James <alazyartist@gmail.com>
2021-08-02 21:30:23 -0600

    Working GLTF loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {Canvas} from '@react-three/fiber';
+import { Suspense } from 'react';
+import Model from './TorqueLogo';
+import {OrbitControls, Environment} from '@react-three/drei';
+import { Camera } from 'three';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
+        <div className="Canvas-container">
+        <Canvas>
+          <Suspense fallback= {null}>
+          <Model/>
+          <OrbitControls/>
+          {/* <mesh  scale={4}>
+          <boxGeometry/>
+          <meshStandardMaterial color="hotpink"/>
+          <ambientLight/>
+           </mesh> */}
+          </Suspense> 
+        </Canvas>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +34,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Powered By Torque
         </a>
-      </header>
+      </div>
     </div>
   );
 }
