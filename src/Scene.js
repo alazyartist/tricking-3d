41e216535944221b65f0c32e29623650c7f23c34
Dylan James <alazyartist@gmail.com>
2021-08-14 15:33:46 -0600

    Removed depricated code, fixed undefined actions issue

--- src/Scene.js
+++ src/Scene.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import {
 	OrbitControls,
@@ -11,64 +11,11 @@ import { useStore } from "./store";
 
 export function TorqueScene(props) {
 	const modelSelector = useStore((state) => state.modelValue);
-	// const [trickCount, setTrickCount] = useState(props.trickCount);
-	// const [currentAnimation, setCurrentAnimation] = useState(
-	// 	props.currentAnimation
-	// );
-	// const [animationArray, setAnimationArray] = useState(props.animationArray);
-	// //UseEffects
-	// useEffect(() => {
-	// 	setAnimationArray(props.animationArray);
-	// }, [props.animationArray, animationArray, props]);
-
-	// useEffect(() => {
-	// 	setCurrentAnimation(props.currentAnimation);
-	// 	console.log("Scene =" + currentAnimation);
-	// }, [props.currentAnimation, currentAnimation, props]);
-
-	// useEffect(() => {
-	// 	setTrickCount(props.trickCount);
-	// }, [props.trickCount, trickCount, props]);
-
-	// // CallBack Funtions
-
-	// const _setAnimationArray = useCallback(
-	// 	(value) => {
-	// 		setAnimationArray(value);
-	// 		console.log("Received arr");
-	// 	},
-	// 	[setAnimationArray]
-	// );
-
-	// const _setCurrentAnimation = useCallback(
-	// 	(value) => {
-	// 		setCurrentAnimation(value);
-	// 	},
-	// 	[setCurrentAnimation]
-	// );
-
-	// const _setTrickCount = useCallback(
-	// 	(value) => {
-	// 		setTrickCount(value);
-	// 		console.log("Trick Count_scene: " + trickCount);
-	// 	},
-	// 	[setTrickCount]
-	// );
 	//Scene Logic -- Switch Models to render: Fred or Torque Logo
 	if (modelSelector) {
 		return (
 			<PerspectiveCamera position={[0, -2, -1]}>
-				<Fred
-				// handleAnimationArray={_setAnimationArray}
-				// currentAnimation={currentAnimation}
-				// handleCurrentAnimation={_setCurrentAnimation}
-				// handleTrickCount={_setTrickCount}
-				// trickCount={trickCount}
-				// index={props.index}
-				// model={props.model}
-				// playing={props.playing}
-				// scale={3}
-				/>
+				<Fred />
 				<ambientLight intensity={1} />
 				<spotLight position={[0, -3, -3]} />
 				<Environment preset='park' />
--- src/store.js
+++ src/store.js
@@ -1,19 +1,25 @@
 import create from "zustand";
+import { devtools } from "zustand/middleware";
 
-export const useStore = create((set) => ({
-	animationsArray: ["Bkick", "Backflip"],
-	modelValue: true,
-	isPlaying: false,
-	setIsPlaying: () => set((state) => ({ isPlaying: !state.isPlaying })),
-	aI: 0,
-	modelArray: [],
-	updateAnimationArray: () =>
-		set((state) => ({
-			animationsArray: state.animationsArray,
-		})),
-	animationSelector: () =>
-		set((state) => ({
-			aI: (state.aI + 1) % state.animationsArray.length,
-		})),
-	modelSelector: () => set((state) => ({ modelValue: !state.modelValue })),
-}));
+export const useStore = create(
+	devtools((set) => ({
+		animationsArray: ["Bkick", "Backflip"],
+		modelValue: true,
+		isPlaying: false,
+		isPaused: true,
+		setIsPlaying: () => set((state) => ({ isPlaying: !state.isPlaying })),
+		setIsPaused: () => set((state) => ({ isPaused: !state.isPaused })),
+		aI: 0,
+		modelArray: [],
+		updateAnimationArray: () =>
+			set((state, value) => ({
+				animationsArray: value,
+				...state.animationsArray,
+			})),
+		animationSelector: () =>
+			set((state) => ({
+				aI: (state.aI + 1) % state.animationsArray.length,
+			})),
+		modelSelector: () => set((state) => ({ modelValue: !state.modelValue })),
+	}))
+);
