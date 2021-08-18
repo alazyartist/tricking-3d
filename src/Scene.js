9eda5e6ccad57d4eac3ec013678e7306c62428fa
Dylan James <alazyartist@gmail.com>
2021-08-18 02:12:42 -0600

    Added Fred2 with HardCoded Animations Array

--- src/Scene.js
+++ src/Scene.js
@@ -6,7 +6,8 @@ import {
 	PerspectiveCamera,
 } from "@react-three/drei";
 import TorqueLogo from "./TorqueLogo";
-import { Fred } from "./AnimatingTest";
+// import { Fred } from "./AnimatingTest";
+import { Fred2 } from "./AnimatingTestIdle.js";
 import { useStore } from "./store";
 
 export function TorqueScene(props) {
@@ -14,9 +15,10 @@ export function TorqueScene(props) {
 	//Scene Logic -- Switch Models to render: Fred or Torque Logo
 	if (modelSelector) {
 		return (
-			<PerspectiveCamera frustumCulled={false}>
-				position={[1, 0, 0]}
-				<Fred position={[-1, 0, 0]} rotation={[0, 20, 0]} />
+			<PerspectiveCamera>
+				{/* <Fred position={[-1, 0, 0]} rotation={[0, 20, 0]} /> */}
+				<Fred2 />
+
 				<ambientLight intensity={0.2} />
 				<spotLight intensity={4} position={[0, -3, -3]} />
 				<Environment preset='park' />
