1de6c462c1bbce10a0ab7b1535a3f748123e6d5b
Dylan James <alazyartist@gmail.com>
2021-11-13 23:27:21 -0700

    Fixed Json Store for TrickInfo

--- src/TrickInfoJson.js
+++ src/TrickInfoJson.js
@@ -1,6 +1,8 @@
 export const TrickInformation = {
 	Au: `Au TEST TEXT. PLZ WRK`,
-	Backflip: `Sample Text about backflips. A backflip flips backwards over the head.
+	BackFlip: `Sample Text about backflips. A backflip flips backwards over the head.
     Typically takes off from two feet and has a range of potentials to
     variate into.`,
+	BadRaiz: `Sample Text for BadRaiz`,
+	Cartwheel: `Cartwheel Sample Text i wonder if i type more will it dissapear interesting not yet but at some pointim sure it will go away`,
 };
