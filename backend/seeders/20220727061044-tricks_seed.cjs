744ee7800aa6a53d381f2323feaa1ab754d9c7c1
Dylan James <alazyartist@gmail.com>
2022-07-28 05:06:25 -0600

    BaseTricks Seed

--- backend/seeders/20220727061044-tricks_seed.cjs
+++ backend/seeders/20220727061044-tricks_seed.cjs
@@ -12,6 +12,8 @@ module.exports = {
 		 * }], {});
 		 */
 		await queryInterface.bulkInsert("Tricks", [
+			//BaseTricks
+			//1
 			{
 				trick_id: uuidv4(),
 				base_id: "Backflip",
@@ -22,6 +24,40 @@ module.exports = {
 				createdAt: new Date(Date.now()),
 				updatedAt: new Date(Date.now()),
 			},
+			//2
+			{
+				trick_id: uuidv4(),
+				base_id: "InsideFlip",
+				name: "InsideFlip",
+				stance_id: "Inside",
+				takeoffStance: "Inside",
+				landingStance: "Inside",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//3
+			{
+				trick_id: uuidv4(),
+				base_id: "Frontflip",
+				name: "Frontflip",
+				stance_id: "Frontside",
+				takeoffStance: "Frontside",
+				landingStance: "Frontside",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//4
+			{
+				trick_id: uuidv4(),
+				base_id: "OutsideFlip",
+				name: "OutsideFlip",
+				stance_id: "Outside",
+				takeoffStance: "Outside",
+				landingStance: "Outside",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//5
 			{
 				trick_id: uuidv4(),
 				base_id: "Gainer",
@@ -32,6 +68,85 @@ module.exports = {
 				createdAt: new Date(Date.now()),
 				updatedAt: new Date(Date.now()),
 			},
+			//6
+			{
+				trick_id: uuidv4(),
+				base_id: "GainerR",
+				name: "DSGainer",
+				stance_id: "BacksideHyper",
+				takeoffStance: "BacksideHyper",
+				landingStance: "BacksideComplete",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+
+			//7
+			{
+				trick_id: uuidv4(),
+				base_id: "GMS",
+				name: "GMS",
+				stance_id: "InsideHyper",
+				takeoffStance: "InsideHyper",
+				landingStance: "InsideHyper",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//8
+			{
+				trick_id: uuidv4(),
+				base_id: "Aerial",
+				name: "Aerial",
+				stance_id: "InsideMega",
+				takeoffStance: "InsideMega",
+				landingStance: "InsideHyper",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//9
+			{
+				trick_id: uuidv4(),
+				base_id: "Webster",
+				name: "Webster",
+				stance_id: "FrontsideMega",
+				takeoffStance: "FrontsideMega",
+				landingStance: "FrontsideSemi",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//10
+			{
+				trick_id: uuidv4(),
+				base_id: "WebsterR",
+				name: "DSWebster",
+				stance_id: "FrontsideSemi",
+				takeoffStance: "FrontsideSemi",
+				landingStance: "FrontsideMega",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//11
+			{
+				trick_id: uuidv4(),
+				base_id: "Raiz",
+				name: "Raiz",
+				stance_id: "OutsideSemi",
+				takeoffStance: "OutsideSemi",
+				landingStance: "BacksideComplete",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//12
+			{
+				trick_id: uuidv4(),
+				base_id: "Lotus",
+				name: "Lotus",
+				stance_id: "OutsideComplete",
+				takeoffStance: "OutsideComplete",
+				landingStance: "OutsideComplete",
+				createdAt: new Date(Date.now()),
+				updatedAt: new Date(Date.now()),
+			},
+			//Corks
 			{
 				trick_id: uuidv4(),
 				base_id: "Gainer",
