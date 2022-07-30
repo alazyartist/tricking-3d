77c6259acbba1eb9e7eff3dde5f75efbde00a22f
Dylan James <alazyartist@gmail.com>
2022-07-30 05:37:16 -0600

    WIRING THE DB

--- backend/seeders/20220727203518-trick_variations.cjs
+++ backend/seeders/20220727203518-trick_variations.cjs
@@ -13,8 +13,33 @@ module.exports = {
 		 */
 		await queryInterface.bulkInsert("Trick_Variations", [
 			{
+				trick_id: "ed832bc6-eae3-458d-8cfd-ragonflycork",
 				variation_id: 9,
-				trick_id: "ed832bc6-eae3-458d-8cf2-26b5b531d96c",
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cfc-26brodeocork",
+				variation_id: 34,
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cfc-orkhyperhook",
+
+				variation_id: 22,
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cfc-orkshuiriken",
+				variation_id: 18,
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cfc-orkmegaround",
+				variation_id: 15,
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cf2-26bcorkround",
+				variation_id: 12,
+			},
+			{
+				trick_id: "ed832bc6-eae3-458d-8cf2-26bcorkswipe",
+				variation_id: 13,
 			},
 		]);
 	},
