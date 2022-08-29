abde3c56ba043e45dced52395640457d981dd457
Dylan James <alazyartist@gmail.com>
2022-08-29 16:06:28 -0600

    Profile Page

--- frontend/src/pages/userProfile/userProfile.js
+++ frontend/src/pages/userProfile/userProfile.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import useUserInfoByUUID from "../../api/useUserInfoById";
+import ProfileCode from "../dash/components/ProfileCode";
 
 const UserProfile = () => {
 	const { uuid } = useParams();
 
-	const { data } = useUserInfoByUUID(uuid);
+	const { data: profileInfo } = useUserInfoByUUID(uuid);
+	console.log(profileInfo);
 
 	return (
 		<div className='flex flex-col place-content-center place-items-center pt-[3.4rem] text-zinc-300'>
-			<div>{JSON.stringify(data)}</div>
-			<div>{uuid}</div>
 			<div>UserProfile</div>
+			<div>{profileInfo?.first_name + " " + profileInfo?.last_name}</div>
+			<div>{uuid}</div>
+			<div>
+				<div>Tricklists</div>
+				{profileInfo?.MyTricklists?.map((list) => (
+					<div className='text-sm text-zinc-400'>{list?.name}</div>
+				))}
+			</div>
 		</div>
 	);
 };
