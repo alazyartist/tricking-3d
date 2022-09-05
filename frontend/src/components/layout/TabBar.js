a33408bf73b6346b6dcb66ce13129d03e1fc626a
Tohzt <steven.g.french@live.com>
2022-09-05 03:15:23 -0700

    cleaned up hide animation

--- frontend/src/components/layout/TabBar.js
+++ frontend/src/components/layout/TabBar.js
@@ -23,26 +23,26 @@ function TabBar() {
 		// onRest: () => setOpenHamburger(!openHamburger),
 	});
 
-	const navToggle = useTransition(openNav, {
-		from: { opacity: 1, bottom: "1" },
-		enter: { opacity: 1, bottom: "1" },
-		leave: { opacity: 1, bottom: "1" },
+	const navToggle = useSpring({
+		from: { bottom: "-38px" },
+		to: { bottom: "0px" },
 		reverse: openNav,
-		delay: 100,
-		config: { durration: 1200, tension: 50, friction: 10 },
-		// onRest: () => setOpenHamburger(!openHamburger),
+		config: {
+			duration: 350,
+			config: config.wobbly,
+		},
 	});
 
 	return (
 		<>
-			{navToggle(
-				(styles, showing) =>
-					showing && (
-						<div>
-					<button className='fixed z-[999] bottom-[45px] left-0 w-[100%] h-4 flex place-content-center place-items-center'
+			<div className='absolute w-[100%] bottom-0 overflow-hidden'>
+				<animated.div style={(navToggle)} className='relative'>
+					<button 
+						style={navToggle} 
+						className='relative z-[999] left-0 w-[100%] h-4 flex place-content-center place-items-center'
 						onClick={() => setOpenNav(!openNav)}>
 					</button>
-					<div className='fixed bottom-[-4px] left-0 z-[100] flex h-12 w-full place-content-center place-items-center gap-8 rounded-t-2xl bg-gradient-to-b from-zinc-900 to-zinc-800 text-2xl text-zinc-300'>
+					<div style={(navToggle)} className='relative left-0 z-[100] flex h-12 w-full place-content-center place-items-center gap-8 rounded-t-2xl bg-gradient-to-b from-zinc-900 to-zinc-800 text-2xl text-zinc-300'>
 						<Link onClick={() => setOpenHamburger(false)} to='/home'>
 							<AiOutlineHome />
 						</Link>
@@ -58,55 +58,30 @@ function TabBar() {
 						</Link>
 						<GiHamburgerMenu onClick={() => setOpenHamburger(!openHamburger)} />
 					</div>
-					</div>
-				)||
-						<div>
-					<button className='fixed z-[999] bottom-[0px] left-0 w-[100%] h-8 flex place-content-center place-items-center'
-						onClick={() => {
-							setOpenNav(!openNav);
-						}}>
-					</button>
-					<div className='fixed bottom-[-36px] left-0 z-[100] flex h-12 w-full place-content-center place-items-center gap-8 rounded-t-2xl bg-gradient-to-b from-zinc-900 to-zinc-800 text-2xl text-zinc-300'>
-						<Link onClick={() => setOpenHamburger(false)} to='/home'>
-							<AiOutlineHome />
-						</Link>
-						<Link onClick={() => setOpenHamburger(false)} to='/comboMaker'>
-							{/* <FaToolbox /> */}
-							<ComboMakerBlueprint className='h-10 w-10' fill='#ffffff' />
-						</Link>
-						<Link onClick={() => setOpenHamburger(false)} to='/sandbox'>
-							<BiCube />
-						</Link>
-						<Link onClick={() => setOpenHamburger(false)} to='/theory'>
-							<FaGraduationCap />
-						</Link>
-						<GiHamburgerMenu onClick={() => setOpenHamburger(!openHamburger)} />
-					</div>
-					</div>
-				)
-			}
+				</animated.div>
+			</div>
 
 			{/* Open Hamburger Menu Display */}
 			{hamburger(
 				(styles, hamburgerMenu) =>
-					hamburgerMenu && (
-						<animated.div
-							id='side-Menu'
-							style={styles}
-							onClick={() => setOpenHamburger(!openHamburger)}
-							className='fixed bottom-14 z-[100] rounded-l-xl'>
-							<animated.div className='flex h-[40vh] max-w-[40vw] flex-col gap-3 rounded-l-xl bg-gradient-to-br from-zinc-900 to-zinc-800 p-3 text-sm text-white'>
-								<Link to='/about'>About</Link>
-								<Link to='/contribute'>Contibute</Link>
-								<Link to='/learnMore'>Learn More</Link>
-								<button
-									className='absolute bottom-2 left-3'
-									onClick={() => logout()}>
-									Logout
-								</button>
-							</animated.div>
+				hamburgerMenu && (
+					<animated.div
+						id='side-Menu'
+						style={styles}
+						onClick={() => setOpenHamburger(!openHamburger)}
+						className='fixed bottom-14 z-[100] rounded-l-xl'>
+						<animated.div className='flex h-[40vh] max-w-[40vw] flex-col gap-3 rounded-l-xl bg-gradient-to-br from-zinc-900 to-zinc-800 p-3 text-sm text-white'>
+							<Link to='/about'>About</Link>
+							<Link to='/contribute'>Contibute</Link>
+							<Link to='/learnMore'>Learn More</Link>
+							<button
+								className='absolute bottom-2 left-3'
+								onClick={() => logout()}>
+								Logout
+							</button>
 						</animated.div>
-					)
+					</animated.div>
+				)
 			)}
 		</>
 	);
