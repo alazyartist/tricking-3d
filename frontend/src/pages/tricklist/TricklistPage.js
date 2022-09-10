a8d85e001a21b72136df7c32ccc2dee717da7801
Tohzt <steven.g.french@live.com>
2022-09-10 00:27:42 -0700

    further improved tricklist code

--- frontend/src/pages/tricklist/TricklistPage.js
+++ frontend/src/pages/tricklist/TricklistPage.js
@@ -1,45 +1,58 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import AddListButton from "./components/AddListButton";
 import ListViewbyID from "./components/ListViewbyID";
 import MakeNewTrickList from "./components/MakeNewTrickList";
-import TrickList_Container from "./components/trickList_Container";
+import TrickList from "./components/trickList"
+import Data_Mock from "../../data/trickList_mock"
 
 const TricklistPage = ({ displayOnly, profileuuid }) => {
 	const [open, setOpen] = useState(false);
 	const [openView, setOpenView] = useState(false);
 	const [addItemopen, setAddItemopen] = useState(false);
+	// @TODO: Uncomment for actual data
+	//const { data: lists } = useGetTricklists(profileuuid);
+	//const { uuid: userUUID } = useUserStore((s) => s.userInfo);
+	//const userInfo = useUserStore((s) => s.userInfo);
+	const [mockedData] = useState(Data_Mock)
+	const [data, setData] = useState(mockedData)
+	const _getDate = (e) => {
+		let date = new Date(e?.createdAt)
+		return (date.toDateString().slice(3, date.length))
+	}
 
-	const [tricklist_id, setTricklist_id] = useState("");
-	useEffect(() => {
-	}, [tricklist_id]);
-
-	//TODO add animations between Tricklist Views
 	return (
 		<div className='h-[500px]'>
-			<div id="trickList_Header"
-				className='h-5 flex flex-row justify-center items-end'>
+			<div className='h-5 flex flex-row justify-center items-end'>
 				Trick List
 			</div>
 
 			<div
-				id={"tricklistPage-Container"}
-				className='
-				bg-zinc-400 border-white 
-				h-full w-[90vw] p-1
-				rounded-md
-				'>
-				<div className='h-full w-full'>
+				className='bg-zinc-400 border-white h-full w-[90vw] p-1 rounded-md'>
+				<div>
 					{!openView && (
-						<div className='flex flex-row'>
-								<TrickList_Container
-									addItemopen={addItemopen}
-									profileuuid={profileuuid}
-									setTricklist_id={setTricklist_id}
-									displayOnly={displayOnly}
-									setOpenView={setOpenView}
-									openView={openView}
-									open={open}
-								/>
+						<div className='h-[500px] w-full bg-zinc-900 gap-[1px] flex flex-col justify-start'>
+							{
+								Array.isArray(data) &&
+									data.length > 0 &&
+									data.map((list, i) => {
+										return (
+											<>
+												{
+													<TrickList
+														key={list.id}
+														data={list}
+														date={_getDate(list)}
+														fn={() => { console.log("List click from within _Container") }}
+														drag_offset={60}
+														swipe_left={() => console.log(list.name, "- Swipe Left: Replace with function")}
+														swipe_right={() => console.log(list.name, "- Swipe Right: Replace with function")}
+													/>
+												}
+											</>
+										)
+									}
+									)
+							}
 						</div>
 					)}
 
@@ -60,8 +73,6 @@ const TricklistPage = ({ displayOnly, profileuuid }) => {
 								setAddItemopen={setAddItemopen}
 								displayOnly={displayOnly}
 								setOpenView={setOpenView}
-								tricklist_id={tricklist_id}
-								setTricklist_id={setTricklist_id}
 								openView={openView}
 								open={open}
 							/>
