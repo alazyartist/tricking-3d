a9c8e21f0a49ecce08f2da23b01c3dc659d7cfdd
Dylan James <alazyartist@gmail.com>
2022-09-29 19:59:39 -0600

    UI CleanUp

--- frontend/src/admin/components/DataList.js
+++ frontend/src/admin/components/DataList.js
@@ -7,13 +7,13 @@ const DataList = () => {
 	const { data: tricks } = useGetTricks();
 	const { data: combos } = useGetCombos();
 	return (
-		<div className='no-scrollbar flex max-h-[50vh] flex-col place-items-center gap-2 overflow-y-scroll rounded-xl pb-14'>
+		<div className='no-scrollbar flex max-h-[50vh] w-[70vw] flex-col place-items-center gap-2 overflow-y-scroll rounded-xl pb-14'>
 			<h1 className='sticky top-0 h-full w-full bg-zinc-800 p-2 text-center text-xl font-bold'>
 				TRICKS, STANCES, &#38; TRANSITIONS
 			</h1>
 			<div>
 				{tricks?.map((trick) => (
-					<div className=' grid  w-[70vw] grid-cols-6 justify-between p-2 odd:bg-zinc-700'>
+					<div className=' grid  w-full grid-cols-6 justify-between p-2 odd:bg-zinc-700'>
 						<div className='col-span-3'>{trick?.name}</div>
 						<div className='col-span-2 flex place-items-center text-sm'>
 							{trick?.type}
--- frontend/src/admin/components/UserList.js
+++ frontend/src/admin/components/UserList.js
@@ -7,12 +7,12 @@ const UserList = () => {
 		console.log(allUsers);
 	}, [allUsers]);
 	return (
-		<div className='no-scrollbar h-[20vh] w-[80vw] overflow-scroll p-4'>
+		<div className='no-scrollbar h-[40vh] w-[80vw] overflow-scroll p-4'>
 			<div>UserList</div>
 			<div className='flex flex-col gap-2 p-2'>
 				{Array.isArray(allUsers) &&
 					allUsers?.map((user) => (
-						<div className='grid grid-cols-3 gap-2 overflow-hidden text-xs odd:bg-zinc-700'>
+						<div className='grid grid-cols-3 gap-2 overflow-hidden whitespace-nowrap text-xs odd:bg-zinc-700'>
 							<div className='overflow-hidden '>{user.username}</div>
 							<div className=''>
 								{user.first_name} {user.last_name}
