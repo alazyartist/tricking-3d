7876435171b3ade685c633a823a46278f9e611fb
Dylan James <alazyartist@gmail.com>
2022-10-07 19:37:14 -0600

    move vidcontrols

--- frontend/src/pages/sandbox/ui/VideoOverlay.js
+++ frontend/src/pages/sandbox/ui/VideoOverlay.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { IoIosVideocam } from "react-icons/io";
 import { useStore } from "../../../store/store";
+import { useVideoStore } from "../videoOverlay/useVideoStore";
 import VideoControls from "../videoOverlay/VideoControls";
 import VideoInput from "../videoOverlay/VideoInput";
 const VideoOverlay = () => {
@@ -12,6 +13,7 @@ const VideoOverlay = () => {
 		setBackground("The Void");
 		setFollowCam(false);
 	};
+	const videoSource = useVideoStore((s) => s.videoSource);
 	return (
 		<>
 			<div className='absolute bottom-[35vh] right-5 z-[1000] '>
@@ -21,9 +23,11 @@ const VideoOverlay = () => {
 				/>
 			</div>
 			{videoInput && <VideoInput />}
-			<div className='absolute top-[10vh] z-[4] flex w-full place-content-center place-items-center gap-2 text-zinc-300'>
-				<VideoControls />
-			</div>
+			{videoSource && (
+				<div className='absolute top-[10vh] z-[4] flex w-full place-content-center place-items-center gap-2 text-zinc-300'>
+					<VideoControls />
+				</div>
+			)}
 		</>
 	);
 };
