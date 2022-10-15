998e93552acb3611855add51bd4c456ebaeb4f0a
Dylan James <alazyartist@gmail.com>
2022-10-14 21:45:17 -0600

    combo filters

--- frontend/src/pages/comboMakerV2/components/Tricks.js
+++ frontend/src/pages/comboMakerV2/components/Tricks.js
@@ -9,6 +9,8 @@ import useComboMakerV2 from "../useComboMakerV2";
 const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 	const [searchTerm, setSearchTerm] = useState("");
 	const [searchedItems, setSearchedItems] = useState();
+	const [typedItems, setTypedItems] = useState();
+	const [activeTypes, setActiveTypes] = useState(["Tricks"]);
 	useEffect(() => {
 		if (
 			(allTricks !== undefined && !searchTerm) ||
@@ -16,7 +18,23 @@ const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 		) {
 			setSearchedItems(allTricks);
 		}
+		console.log(activeTypes);
 	}, [allTricks, searchedItems]);
+	useEffect(() => {
+		typeFilter(searchedItems);
+	}, [activeTypes, searchedItems, allTricks]);
+	const typeFilter = (arr) => {
+		if (activeTypes.length) {
+			activeTypes?.map((type) => {
+				let temp = arr?.filter((value) => {
+					console.log(value.type);
+					return value.type.toLowerCase().includes(type.toLowerCase());
+				});
+				console.log(temp);
+				setTypedItems(temp);
+			});
+		}
+	};
 	const handleFilter = (event) => {
 		const searchTerm = event.target.value;
 		const newFilter = allTricks.filter((value) => {
@@ -25,7 +43,15 @@ const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 		setSearchTerm(searchTerm);
 		setSearchedItems(newFilter);
 	};
-
+	const handleAddType = (type) => {
+		if (activeTypes?.includes(type)) {
+			console.log("already in there");
+			return;
+		} else {
+			console.log("adding");
+			setActiveTypes([...activeTypes, type]);
+		}
+	};
 	return (
 		<>
 			<div
@@ -50,6 +76,7 @@ const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 					onChange={handleFilter}
 				/>
 			</div>
+
 			<div className='no-scrollbar flex h-[60vh] w-[60vw] max-w-[560px] flex-col gap-3 overflow-y-auto rounded-xl p-2 peer-hover:bg-red-500'>
 				{/* TODO Current Leg should go here-ish */}
 				<p className='place-self-end text-sm text-zinc-500'>
@@ -63,8 +90,8 @@ const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 				<p className='place-self-end text-sm text-zinc-500'>
 					{filteredTricks.length} Options
 				</p>
-				{searchedItems?.length &&
-					searchedItems?.map((trick) => (
+				{typedItems?.length > 0 &&
+					typedItems?.map((trick) => (
 						<div
 							key={trick.trick_id + Math.random().toString()}
 							className='rounded-xl  bg-zinc-800 p-2'>
@@ -82,6 +109,23 @@ const Tricks = ({ allTricks, lastItem, setCurrentItem, filteredTricks }) => {
 							</div>
 						</div>
 					))}
+			</div>
+			<div className='absolute left-4 bottom-[20vh] flex w-fit flex-col gap-2 text-xs font-bold'>
+				<div
+					onClick={() => handleAddType("Trick")}
+					className='flex h-16 place-content-center place-items-center rounded-xl bg-zinc-700 p-1'>
+					Tricks
+				</div>
+				<div
+					onClick={() => handleAddType("Transition")}
+					className='flex h-16 place-content-center  place-items-center rounded-xl bg-zinc-700 p-1'>
+					Transitions
+				</div>
+				<div
+					onClick={() => handleAddType("Stance")}
+					className='flex h-16 place-content-center place-items-center rounded-xl bg-zinc-700 p-1'>
+					Stances
+				</div>
 			</div>
 			{/* <div className='absolute bottom-16 flex gap-2'>
 				<MdOutlineArrowUpward />
