7e311e39ff2965bae173e2c926d92583b223392c
Dylan James <alazyartist@gmail.com>
2022-10-22 22:47:10 -0600

    battleroomRoute

--- backend/routes/battleroom.routes.js
+++ backend/routes/battleroom.routes.js
@@ -4,6 +4,7 @@ import {
 	getRooms,
 	makeNewRoom,
 	setRoomInactive,
+	updateRoomScore,
 	updateRoomStats,
 } from "../controllers/battleroom.controller.js";
 
@@ -12,5 +13,6 @@ export const battleroomRoutes = express.Router();
 battleroomRoutes.get("/", getRooms);
 battleroomRoutes.post("/", makeNewRoom);
 battleroomRoutes.post("/:sessionid", updateRoomStats);
+battleroomRoutes.post("/:sessionid/score", updateRoomScore);
 battleroomRoutes.put("/:sessionid", setRoomInactive);
 battleroomRoutes.get("/:sessionid", getRoombySessionid);
