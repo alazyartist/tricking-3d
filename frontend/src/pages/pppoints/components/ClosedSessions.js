99e821767fc97b6d9776605ca40f837c3d9333a6
Dylan James <alazyartist@gmail.com>
2022-10-23 23:40:35 -0600

    roomsDisplay

--- frontend/src/pages/pppoints/components/ClosedSessions.js
+++ frontend/src/pages/pppoints/components/ClosedSessions.js
@@ -1,32 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useGetBattleRooms } from "../../../api/useBattleRoom";
+import { IoCheckmark, IoChevronBack, IoChevronForward } from "react-icons/io5";
+import { FaCheck, FaGavel } from "react-icons/fa";
+import { useUserStore } from "../../../store/userStore";
 
 const ClosedSessions = () => {
 	const { data: battleRooms } = useGetBattleRooms();
+	const userUUID = useUserStore((s) => s.userInfo.uuid);
+
+	const [dateFilter, setDateFilter] = useState(
+		new Date(Date.now()).toDateString()
+	);
+	function getNewDate(date, days) {
+		let d = new Date(date);
+		d.setDate(d.getDate() + days);
+		setDateFilter(d.toDateString());
+	}
+	console.log();
 	return (
 		<div>
 			<div>Battle Results</div>
-			<div className='flex max-h-[50vh] flex-col overflow-y-auto rounded-md bg-zinc-900 p-1 font-normal text-zinc-300'>
+
+			<span className='my-2 flex w-full place-content-center place-items-center gap-2'>
+				<IoChevronBack
+					className='rounded-md bg-zinc-900 p-1 text-xl'
+					onClick={() => getNewDate(dateFilter, -1)}
+				/>
+				{dateFilter}
+				<IoChevronForward
+					className='rounded-md bg-zinc-900 p-1 text-xl'
+					onClick={() => getNewDate(dateFilter, 1)}
+				/>
+			</span>
+			<div className='flex max-h-[50vh] flex-col gap-2 overflow-y-auto rounded-md font-normal text-zinc-300'>
 				{(battleRooms?.length < 1 ||
 					battleRooms?.every((r) => r.isOpen == true)) &&
 					"No Sessions Available"}
 				{battleRooms?.map((room) => {
-					if (!room.isOpen) {
+					let date = new Date(room.updatedAt).toDateString();
+					if (!room.isOpen && dateFilter === date) {
 						return (
-							<Link to={`stats/${room.sessionid}`}>
-								{room?.team1?.map((user) => (
-									<span key={user.uuid}>
-										{user.username} {room?.team1.length > 1 && "&"}{" "}
-									</span>
-								))}{" "}
-								VERSUS{" "}
-								{room?.team2?.map((user) => (
-									<span>
-										{user.username}
-										{room?.team2.length > 1 && "&"}{" "}
-									</span>
-								))}
+							<Link
+								className='flex w-full place-items-center justify-around gap-2 rounded-md bg-zinc-900 p-2'
+								to={`stats/${room.sessionid}`}>
+								<span>
+									{room?.team1?.map((user) => (
+										<span key={user.uuid}>
+											{user.username} {room?.team1.length > 1 && "&"}{" "}
+										</span>
+									))}
+									<span className='text-xs text-zinc-400'> vs </span>
+									{room?.team2?.map((user) => (
+										<span>
+											{user.username}
+											{room?.team2.length > 1 && "&"}{" "}
+										</span>
+									))}
+								</span>
+								<span className='flex gap-2 text-xs text-zinc-500'>
+									{room.judges.some((j) => j.uuid === userUUID) && (
+										<FaGavel className='text-emerald-500' />
+									)}
+								</span>
+								<span className='flex gap-2 text-xs text-zinc-500'>
+									{room.host === userUUID && (
+										<FaCheck className='text-emerald-500' />
+									)}
+								</span>
 							</Link>
 						);
 					}
--- frontend/src/pages/pppoints/components/LiveSessions.js
+++ frontend/src/pages/pppoints/components/LiveSessions.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useGetBattleRooms } from "../../../api/useBattleRoom";
-
+import { useUserStore } from "../../../store/userStore";
+import { FaGavel } from "react-icons/fa";
 const LiveSessions = ({ ably }) => {
 	const liveSessionsChannel = ably?.channels.get("[?rewind=10m]LiveSessions");
+	const userUUID = useUserStore((s) => s.userInfo.uuid);
 
 	const [liveSessionsFeed, updateLiveSessionsFeed] = useState([]);
 	const { data: battleRooms } = useGetBattleRooms();
@@ -28,7 +30,7 @@ const LiveSessions = ({ ably }) => {
 	return (
 		<div>
 			<div>Live Sessions</div>
-			<div className='flex flex-col rounded-md bg-zinc-900 p-1 font-normal text-zinc-300'>
+			<div className='flex max-h-[50vh] flex-col gap-2 overflow-y-auto rounded-md font-normal text-zinc-300'>
 				{liveSessionsFeed?.length < 1 &&
 					(battleRooms?.length < 1 ||
 						battleRooms?.every((r) => r.isOpen == false)) &&
@@ -36,19 +38,28 @@ const LiveSessions = ({ ably }) => {
 				{battleRooms?.map((room) => {
 					if (room.isOpen) {
 						return (
-							<Link to={`${room.sessionid}`}>
-								{room?.team1?.map((user) => (
-									<span key={user.uuid}>
-										{user.username} {room?.team1.length > 1 && "&"}{" "}
-									</span>
-								))}{" "}
-								VERSUS{" "}
-								{room?.team2?.map((user) => (
-									<span>
-										{user.username}
-										{room?.team2.length > 1 && "&"}{" "}
-									</span>
-								))}
+							<Link
+								className='flex w-full place-items-center justify-around gap-2 rounded-md bg-zinc-900 p-2'
+								to={`${room.sessionid}`}>
+								<span>
+									{room?.team1?.map((user) => (
+										<span key={user.uuid}>
+											{user.username} {room?.team1.length > 1 && "&"}{" "}
+										</span>
+									))}
+									<span className='text-xs text-zinc-400'>vs</span>
+									{room?.team2?.map((user) => (
+										<span>
+											{user.username}
+											{room?.team2.length > 1 && "&"}{" "}
+										</span>
+									))}
+								</span>
+								<span>
+									{room.judges.some((j) => j.uuid === userUUID) && (
+										<FaGavel className='text-emerald-500' />
+									)}
+								</span>
 							</Link>
 						);
 					}
