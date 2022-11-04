aaa7f6eff7a9b519cfdfc1e7a353741e1cc1a97a
Dylan James <alazyartist@gmail.com>
2022-11-04 14:39:51 -0600

    multiple urlSources

--- frontend/src/pages/sessions/AddSessionPage.js
+++ frontend/src/pages/sessions/AddSessionPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddSessionPage = () => {
 	const whatsToday = () => {
@@ -8,21 +8,46 @@ const AddSessionPage = () => {
 		)}-${("0" + today.getDate()).slice(-2)}`;
 	};
 	let todaytime = new Date(Date.now()).toISOString().slice(-13, -8);
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log("submitting", e);
+	};
+	const [count, setCount] = useState(1);
 	return (
 		<div className='mt-14 flex flex-col items-center text-zinc-300'>
 			<div className='p-2 text-center font-titan text-2xl'>Submit Session</div>
-			<form className='flex w-[80vw] flex-col gap-2 rounded-md bg-zinc-700 p-3'>
+			<form
+				onSubmit={onSubmit}
+				className='flex w-[80vw] flex-col gap-2 rounded-md bg-zinc-700 p-3'>
+				{Array.from(Array(count).keys()).map((i) => (
+					<div className='flex w-full gap-2 rounded-md bg-zinc-900 bg-opacity-80 p-1 text-zinc-300'>
+						<input
+							id='url'
+							className='w-full bg-transparent p-1 text-zinc-300'
+							type='url'
+							placeholder={`https://your-video.com/goes-here${i}`}
+						/>
+						<span
+							className='text-2xl leading-none text-red-500'
+							onClick={() => setCount((c) => c - 1)}>
+							-
+						</span>
+					</div>
+				))}
+				<span
+					onClick={() => setCount((c) => c + 1)}
+					className='flex place-items-center gap-2 font-titan'>
+					<span>Add Another URL Source</span>
+					<span className='text-2xl leading-none text-emerald-500'>+</span>
+				</span>
 				<input
-					className='rounded-md bg-zinc-900 bg-opacity-80 p-1 text-zinc-300'
-					type='url'
-					placeholder='https://your-video.com/goes-here'
-				/>
-				<input
+					id='name'
 					type='text'
 					className='rounded-md bg-zinc-900 bg-opacity-80 p-1 text-zinc-300'
 					placeholder='Session Name'
 				/>
 				<input
+					id='date'
 					type='date'
 					value={whatsToday()}
 					className='rounded-md bg-zinc-900 bg-opacity-80 p-1 text-zinc-300'
