48694cc4bf6f0ddf5a593984e4cc91db34a84029
Dylan James <alazyartist@gmail.com>
2022-11-05 02:59:02 -0600

    imporve layout

--- frontend/src/admin/components/AdminSessionReview.js
+++ frontend/src/admin/components/AdminSessionReview.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { Link, useParams } from "react-router-dom";
 import ReactPlayer from "react-player";
@@ -8,7 +8,7 @@ const AdminSessionReview = () => {
 	const { sessionid } = useParams();
 	const { data } = useGetSessionDetailsbySessionid(sessionid);
 	const sessionDetails = data?.data;
-	console.log(sessionDetails);
+	// console.log(sessionDetails);
 	return (
 		<div>
 			<Link to={-1}>
@@ -17,25 +17,9 @@ const AdminSessionReview = () => {
 			{sessionDetails && (
 				<div className='mt-8 flex flex-col place-items-center text-zinc-300'>
 					<div>{sessionDetails?.name}</div>
-					<div>
+					<div className='absolute left-2 top-[20vh]'>
 						{sessionDetails?.SessionSources?.map((source) => (
-							<div className='w-[80vw]'>
-								<div>
-									{source?.vidsrc.replace(/^(\w+).*\.com\/watch\?v=/g, "")}
-								</div>
-								<ReactPlayer
-									config={{ facebook: { appId: "508164441188790" } }}
-									id={"video"}
-									controls={false}
-									muted
-									width={"80vw"}
-									height={"40vw"}
-									loop
-									playsInline
-									className={`h-full w-full`}
-									url={source?.vidsrc}
-								/>
-							</div>
+							<SessionSourceDisplay source={source} />
 						))}
 					</div>
 				</div>
@@ -45,3 +29,39 @@ const AdminSessionReview = () => {
 };
 
 export default AdminSessionReview;
+
+const SessionSourceDisplay = ({ source }) => {
+	const [playerVisible, setPlayerVisible] = useState(false);
+	return (
+		<div key={source.srcid + "1"} className=''>
+			{playerVisible ? (
+				<div className='absolute top-[-12vh] left-[15vw] w-[80vw]'>
+					<div onClick={() => setPlayerVisible(!playerVisible)}>
+						{source?.vidsrc.replace(/^(\w+).*\.com\/watch\?v=/g, "")}
+					</div>
+					<ReactPlayer
+						config={{ facebook: { appId: "508164441188790" } }}
+						id={"video"}
+						controls={false}
+						muted
+						width={"70vw"}
+						height={"39.5vw"}
+						loop
+						playsInline
+						className={`h-full w-full`}
+						url={source?.vidsrc}
+					/>
+				</div>
+			) : (
+				<div
+					key={source?.vidsrc.replace(/^(\w+).*\.com\/watch\?v=/g, "")}
+					className='flex flex-col gap-2 bg-zinc-900'>
+					<div onClick={() => setPlayerVisible(!playerVisible)}>
+						{source?.vidsrc.replace(/^(\w+).*\.com\/watch\?v=/g, "")}
+					</div>
+					<div onClick={() => setPlayerVisible(true)}>Show Player</div>
+				</div>
+			)}
+		</div>
+	);
+};
