7eb1982ed060c81a4a88a66cc59c31d128438dc4
Dylan James <alazyartist@gmail.com>
2022-11-07 23:54:21 -0700

    active clip display

--- frontend/src/admin/components/sessionreview/ActiveClipDisplay.js
+++ frontend/src/admin/components/sessionreview/ActiveClipDisplay.js
@@ -1,11 +1,15 @@
 import { animated, useSpring } from "react-spring";
 import React, { useEffect, useState } from "react";
 import { useSessionSummariesStore } from "./SessionSummaryStore";
+import { useParams } from "react-router-dom";
 
 const ActiveClipDisplay = () => {
 	const activeClipData = useSessionSummariesStore((s) => s.clipData);
 	const currentTime = useSessionSummariesStore((s) => s.currentTime);
+	const setSeekTime = useSessionSummariesStore((s) => s.setSeekTime);
 
+	const vidsrc = useSessionSummariesStore((s) => s.vidsrc);
+	const { sessionid } = useParams();
 	const setActiveClipData = useSessionSummariesStore((s) => s.setClipData);
 	const clipDetailsVisible = useSessionSummariesStore(
 		(s) => s.clipDetailsVisible
@@ -28,27 +32,34 @@ const ActiveClipDisplay = () => {
 	return (
 		<animated.div
 			key={activeClipData?.sessionid + "details"}
-			onClick={() => {
-				setClipDetailsVisible();
-				console.log(activeClipData);
-			}}
 			style={{ right: showDetails.right, opacity: showDetails.opacity }}
 			className='absolute top-[20vh] right-2 flex w-[139px] flex-col gap-2 rounded-md rounded-r-none bg-zinc-700 p-1 pl-6 font-inter text-xs text-zinc-300'>
+			<div
+				onClick={() => {
+					setClipDetailsVisible();
+					console.log(activeClipData);
+				}}
+				className='absolute left-[-4px] h-[200px] w-[25px] '
+			/>
+			<div>clipLabel</div>
 			<div>{activeClipData?.name}</div>
 			<div>{activeClipData?.user_id?.slice(-4)}</div>
-			<div>clipLabel</div>
-			<div>sessionid</div>
-			<div>srcid</div>
+			<div>{sessionid}</div>
+			<div>{vidsrc}</div>
 			<div className='flex gap-2'>
-				<div className='rounded-md bg-emerald-300 p-1 text-zinc-800'>
+				<div
+					onClick={() => setSeekTime(activeClipData?.startTime)}
+					className='rounded-md bg-emerald-300 p-1 text-zinc-800'>
 					{activeClipData?.startTime}
 				</div>
-				<div className='rounded-md bg-red-300  p-1 text-zinc-800'>
+				<div
+					onClick={() => setSeekTime(activeClipData?.endTime)}
+					className='rounded-md bg-red-300  p-1 text-zinc-800'>
 					{activeClipData?.endTime}
 				</div>
-				<div className='rounded-md bg-zinc-300  p-1 text-zinc-800'>
-					{Math.floor(currentTime)}
-				</div>
+			</div>
+			<div className='rounded-md bg-zinc-300  p-1 text-zinc-800'>
+				{Math.floor(currentTime)}
 			</div>
 			<animated.span
 				style={{ opacity: showDetails.spanOpacity }}
