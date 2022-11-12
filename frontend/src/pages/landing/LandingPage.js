d8f3aa533c2c94075b4fa2fed7405cd5dd82b64e
Dylan James <alazyartist@gmail.com>
2022-11-12 02:18:22 -0700

    langin page animation

--- frontend/src/pages/landing/LandingPage.js
+++ frontend/src/pages/landing/LandingPage.js
@@ -78,31 +78,31 @@ export default LandingPage;
 export const MovingBackground = () => {
 	const anim = useSpring({
 		loop: true,
+
 		to: [
-			{ o1: 0.8, o2: 0.8, o3: 0.8, l1: "40vw", l2: "40vw", l3: "40vw" },
+			{ o1: 0.8, o2: 0.8, o3: 0.8, l1: "40vw", l2: "60vw", l3: "40vw" },
 			{ o1: 0.5, o2: 0.45, o3: 0.8, l1: "40vw", l2: "20vw", l3: "40vw" },
-			{ o1: 0.45, o2: 0.5, o3: 0.8, l1: "60vw", l2: "40vw", l3: "20vw" },
-			{ o1: 0.75, o2: 0.8, o3: 0.5, l1: "20vw", l2: "60vw", l3: "40vw" },
-			{ o1: 0.8, o2: 0.75, o3: 0.5, l1: "60vw", l2: "40vw", l3: "20vw" },
+
+			{ o1: 0.8, o2: 0.75, o3: 0.5, l1: "80vw", l2: "40vw", l3: "20vw" },
 			{ o1: 0.8, o2: 0.8, o3: 0.8, l1: "40vw", l2: "20vw", l3: "60vw" },
 		],
 		from: { o1: 1, o2: 1, o3: 1, l1: "40vw", l2: "20vw", l3: "60vw" },
-		config: { tension: 65, mass: 1, duration: 1500, friction: 20 },
+		config: { bounce: 10, tension: 25, mass: 1.2, friction: 25 },
 		// onRest: () => setOpenHamburger(!openHamburger),
 	});
 	return (
 		<>
 			<animated.div
-				style={{ opacity: anim.o1, top: anim.l1 }}
-				className={`absolute top-[55vh] left-[60vw] -z-10 h-[369px] w-[369px] translate-x-[-50%] rounded-full bg-teal-300 blur-3xl`}
+				style={{ opacity: anim.o1, top: anim.l1, left: anim.l3 }}
+				className={`absolute top-[55vh] left-[60vw] -z-10 h-[369px] w-[369px] translate-x-[-50%] rounded-full bg-teal-300 blur-3xl md:h-[60vw] md:w-[60vw]`}
 			/>
 			<animated.div
-				style={{ opacity: anim.o2, top: anim.l2 }}
-				className={`absolute top-[35vh] left-[20vw] -z-10 h-[369px] w-[369px] translate-x-[-50%] rounded-full bg-sky-300 blur-3xl`}
+				style={{ opacity: anim.o2, left: anim.l2, top: anim.l1 }}
+				className={`absolute top-[35vh] left-[20vw] -z-10 h-[469px] w-[469px] translate-x-[-50%] rounded-full bg-sky-300 blur-3xl md:h-[60vw] md:w-[60vw]`}
 			/>
 			<animated.div
-				style={{ opacity: anim.o3, top: anim.l3 }}
-				className={`absolute top-[15vh] left-[60vw] -z-10 h-[369px] w-[369px] translate-x-[-50%] rounded-full bg-emerald-300 blur-3xl`}
+				style={{ opacity: anim.o3, top: anim.l3, left: anim.l1 }}
+				className={`absolute top-[15vh] left-[60vw] -z-10 h-[369px] w-[369px] translate-x-[-50%] rounded-full bg-emerald-300 blur-3xl md:h-[60vw] md:w-[60vw]`}
 			/>
 		</>
 	);
