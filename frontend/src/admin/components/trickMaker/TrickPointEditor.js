baef471b7475c0bb0dd299b992b096b0829fab6e
Dylan James <alazyartist@gmail.com>
2022-11-14 20:01:18 -0700

    notify updates

--- frontend/src/admin/components/trickMaker/TrickPointEditor.js
+++ frontend/src/admin/components/trickMaker/TrickPointEditor.js
@@ -3,25 +3,56 @@ import useGetTricks, {
 	useGetTrickParts,
 	useUpdateTrickPoints,
 } from "../../../api/useGetTricks";
-import { MdSave } from "../../../data/icons/MdIcons";
+import { MdCheckCircle, MdSave } from "../../../data/icons/MdIcons";
 import useDebounce from "../../../hooks/useDebounce";
 
 const TrickPointEditor = () => {
 	const { data: trickParts, isLoading } = useGetTrickParts();
 	const { data: tricks } = useGetTricks();
 	if (isLoading) return <p>Loading...</p>;
+	console.log(window?.screen?.orientation?.angle);
 	return (
-		<div className='no-scrollbar h-[80vh] w-[70vw] overflow-hidden overflow-y-scroll md:w-[80vw]'>
-			{trickParts?.length &&
-				trickParts?.map((trick) => {
-					return <PointInput trick={trick} />;
-				})}
-			{tricks?.length &&
-				tricks
-					?.filter((t) => t.type === "Transition")
-					.map((trick) => {
-						return <PointInput trick={trick} />;
-					})}
+		<div
+			className={`no-scrollbar grid ${
+				window?.screen?.orientation?.angle > 0 ? "grid-cols-3" : "grid-cols-1"
+			} h-[80vh] w-[70vw] gap-3 overflow-hidden overflow-y-scroll text-xs md:w-[80vw] md:flex-wrap`}>
+			<div className='h-[70vh] w-[90%] min-w-[100px] overflow-y-scroll'>
+				<div className='sticky top-0 bg-zinc-900 text-2xl'>Bases</div>
+				{trickParts?.length &&
+					trickParts
+						?.filter((a) => !a.type)
+						?.sort((a, b) => {
+							if (a.name > b.name) return 1;
+							if (a.name < b.name) return -1;
+							return 0;
+						})
+						?.map((trick) => {
+							return <PointInput trick={trick} />;
+						})}
+			</div>
+			<div className='h-[70vh] w-[90%] min-w-[100px] overflow-y-scroll'>
+				<div className='sticky top-0 bg-zinc-900 text-2xl'>Stances</div>
+				{trickParts?.length &&
+					trickParts
+						?.filter((a) => a.type === "Stance")
+						?.sort((a, b) => {
+							if (a.name > b.name) return 1;
+							if (a.name < b.name) return -1;
+							return 0;
+						})
+						?.map((trick) => {
+							return <PointInput trick={trick} />;
+						})}
+			</div>
+			<div className='h-[70vh] w-[90%] min-w-[100px] overflow-y-scroll'>
+				<div className='sticky top-0 bg-zinc-900 text-2xl'>Transitions</div>
+				{tricks?.length &&
+					tricks
+						?.filter((t) => t.type === "Transition")
+						?.map((trick) => {
+							return <PointInput trick={trick} />;
+						})}
+			</div>
 			<MdSave className={"absolute top-4 right-4 text-7xl"} />
 		</div>
 	);
@@ -32,8 +63,17 @@ export default TrickPointEditor;
 const PointInput = ({ trick }) => {
 	const [pointValue, setPointValue] = useState(trick?.pointValue);
 	//updatepointValue
-	const { mutate: updatePoints } = useUpdateTrickPoints();
+	const { mutate: updatePoints, data } = useUpdateTrickPoints();
 	const debouncedValue = useDebounce(pointValue, 500);
+	const [saved, setSaved] = useState(false);
+	useEffect(() => {
+		if (data?.status === 200) {
+			setSaved(true);
+			setTimeout(() => setSaved(false), 2000);
+		} else {
+			setSaved(false);
+		}
+	}, [data]);
 	useEffect(() => {
 		if (pointValue !== trick?.pointValue) {
 			if (trick.type === "Transition") {
@@ -72,6 +112,9 @@ const PointInput = ({ trick }) => {
 			<div onClick={() => console.log(trick)} className='w-1/4'>
 				{trick?.name}
 			</div>
+			{saved && (
+				<MdCheckCircle className={"relative left-2 text-emerald-500"} />
+			)}
 			<input
 				on
 				onChange={(e) => setPointValue(e.target.value)}
