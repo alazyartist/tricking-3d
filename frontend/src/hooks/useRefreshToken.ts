0d7cbd88ca4694d1cc3cb1b5151ae824382dc2bf
Dylan James <alazyartist@gmail.com>
2022-11-26 21:59:29 -0700

    typescript conversion

---
+++ frontend/src/hooks/useRefreshToken.ts
@@ -0,0 +1,33 @@
+import React from "react";
+import api, { apiPrivate } from "../api/api";
+import { useUserStore } from "../store/userStore";
+import useLogout from "./useLogout";
+const useRefreshToken = () => {
+	const setAccessToken = useUserStore((s) => s.setAccessToken);
+	const accessToken = useUserStore((s) => s.accessToken);
+	const logout = useLogout();
+	const refresh = async () => {
+		api.defaults.withCredentials = true;
+		const res = await api
+			.get("/refresh", {
+				headers: {
+					"Content-Type": "application/json",
+				},
+				withCredentials: true,
+			})
+			.then((response) => {
+				setAccessToken(response.data.accessToken);
+				console.log("refreshResponse", response.data.accessToken);
+				return response.data.accessToken;
+			})
+			.catch((err) => {
+				logout();
+				console.log("refreshErr", err);
+			});
+		return res;
+	};
+
+	return refresh;
+};
+
+export default useRefreshToken;
