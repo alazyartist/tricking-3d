6e18e267caed5ff97e70651c4a07ba50e4a672dd
Dylan James <alazyartist@gmail.com>
2022-11-29 15:40:53 -0700

    userIcon bug

--- frontend/src/components/app_layout/UserIcon.tsx
+++ frontend/src/components/app_layout/UserIcon.tsx
@@ -1,30 +1,33 @@
+"use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
-// import { Link, useLocation } from "react-router-dom";
-import { TrickedexLogo } from "../../data/icons/TrickedexLogo";
+import React, { useEffect, useState } from "react";
 import { useUserStore } from "../../store/userStore";
 
 const UserIcon = () => {
   const pathname = usePathname();
-  if (typeof window === "undefined") return;
   const { profilePic, uuid, username } = useUserStore((s) => s.userInfo);
-  // const location = useLocation();
+  const [src, setSrc] = useState("/images/noimg.jpeg");
+  const [href, setHref] = useState("/home");
+
+  useEffect(() => {
+    if (uuid && profilePic) {
+      setSrc(`/images/${uuid}/${profilePic}`);
+      if (pathname.includes("/userProfile")) {
+        setHref(`/userProfile/${uuid}`);
+      } else {
+        setHref("/home");
+      }
+    }
+  }, [uuid, profilePic]);
   return (
     username !== null &&
-    uuid !== undefined && (
+    uuid !== (undefined || null) && (
       <Link
-        href={
-          pathname.includes("/userProfile") ? "/home" : `/userProfile/${uuid}`
-        }
+        href={href}
         className="fixed top-2.5 right-5 z-[1002] h-[50px] w-[50px] rounded-full border-2 border-zinc-300 border-opacity-20"
       >
-        <img
-          src={
-            profilePic ? `/images/${uuid}/${profilePic}` : `/images/noimg.jpeg`
-          }
-          className="h-full w-full rounded-full"
-        />
+        <img src={src} className="h-full w-full rounded-full" />
       </Link>
     )
   );
--- frontend/src/components/layout/UserIcon.tsx
+++ frontend/src/components/layout/UserIcon.tsx
@@ -1,30 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TrickedexLogo } from "../../data/icons/TrickedexLogo";
 import { useUserStore } from "../../store/userStore";
 
 const UserIcon = () => {
   const router = useRouter();
   const { profilePic, uuid, username } = useUserStore((s) => s.userInfo);
-  // const location = useLocation();
+  const [src, setSrc] = useState("/images/noimg.jpeg");
+  const [href, setHref] = useState("/home");
+  useEffect(() => {
+    if (uuid && profilePic) {
+      setSrc(`/images/${uuid}/${profilePic}`);
+      if (router.pathname.includes("/userProfile")) {
+        setHref(`/userProfile/${uuid}`);
+      } else {
+        setHref("/home");
+      }
+    }
+  }, [uuid, profilePic]);
   return (
     username !== null &&
     uuid !== undefined && (
       <Link
-        href={
-          router.pathname.includes("/userProfile")
-            ? "/home"
-            : `/userProfile/${uuid}`
-        }
+        href={href}
         className="fixed top-2.5 right-5 z-[1002] h-[50px] w-[50px] rounded-full border-2 border-zinc-300 border-opacity-20"
       >
-        <img
-          src={
-            profilePic ? `/images/${uuid}/${profilePic}` : `/images/noimg.jpeg`
-          }
-          className="h-full w-full rounded-full"
-        />
+        <img src={src} className="h-full w-full rounded-full" />
       </Link>
     )
   );
