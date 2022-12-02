ce9115e27790615ed4acde265d5847747b005266
Dylan James <alazyartist@gmail.com>
2022-12-01 17:16:27 -0700

    convert svg -> tsx

---
+++ frontend/src/data/IndividualStances/Backside.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const Backside = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
+    viewBox="0 0 1080 1080"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>
+      {
+        ".st0{fill:#7ee0fb}.st2{fill:#6bcee9}.st4{fill:#5ed8c5}.st7{fill:#003eb3}.st8{fill:#06d8b7}.st9{fill:#10b35d}.st10{fill:#75fbb3}.st16{fill:#7accbf}.st17{fill:#83c5d7}.st18{fill:#a4e6f8}.st19{fill:#9ef8c8}"
+      }
+    </style>
+    <path
+      d="M540 940c-110.5 0-210.5-44.8-282.8-117.2L340 740l200 200z"
+      style={{
+        fill: "#1e7848",
+      }}
+    />
+    <path
+      d="m340 740-82.8 82.8C184.8 750.5 140 650.5 140 540l200 200z"
+      style={{
+        fill: "#1d3c78",
+      }}
+    />
+    <path
+      d="M340 340 140 540c0-110.5 44.8-210.5 117.2-282.8l79.2 79.2 3.6 3.6z"
+      style={{
+        fill: "#4565a1",
+      }}
+    />
+    <path
+      d="M540 140 340 340l-3.7-3.7-79.2-79.2C329.5 184.8 429.5 140 540 140z"
+      style={{
+        fill: "#449689",
+      }}
+    />
+    <path
+      style={{
+        fill: "#40baa6",
+      }}
+      d="M540 140v400L340 340z"
+    />
+    <path
+      d="M940 540c0 110.5-44.8 210.5-117.2 282.8S650.5 940 540 940V140c110.5 0 210.5 44.8 282.8 117.2C895.2 329.5 940 429.5 940 540z"
+      style={{
+        fill: "#07b9e9",
+      }}
+    />
+    <path
+      style={{
+        fill: "#2db36c",
+      }}
+      d="M540 540v400L340 740z"
+    />
+    <path
+      style={{
+        fill: "#2b5ab3",
+      }}
+      d="M540 540 340 740 140 540z"
+    />
+    <path
+      style={{
+        fill: "#4171ca",
+      }}
+      d="M540 540H140l200-200z"
+    />
+  </svg>
+);
+
+export default Backside;
---
+++ frontend/src/data/IndividualStances/FrontsideMega.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const FrontsideMega = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    x={0}
+    y={0}
+    viewBox="0 0 1080 1080"
+    xmlSpace="preserve"
+    {...props}
+  >
+    <style>
+      {
+        ".st3{fill:#2b5ab3}.st5{fill:#2db36c}.st6{fill:#07b9e9}.st7{fill:#003eb3}.st8{fill:#06d8b7}.st9{fill:#10b35d}.st11{fill:#40baa6}.st12{fill:#1e7848}.st13{fill:#1d3c78}.st14{fill:#4565a1}.st15{fill:#449689}"
+      }
+    </style>
+    <path
+      d="M822.9 257.2 740 340 540 140c110.5 0 210.5 44.8 282.9 117.2z"
+      style={{
+        fill: "#7accbf",
+      }}
+    />
+    <path
+      d="M940 540 740 340l82.8-82.8C895.2 329.5 940 429.5 940 540z"
+      style={{
+        fill: "#83c5d7",
+      }}
+    />
+    <path
+      d="M940 540c0 110.5-44.8 210.5-117.2 282.8l-79.2-79.2-3.6-3.6 200-200z"
+      style={{
+        fill: "#a4e6f8",
+      }}
+    />
+    <path
+      d="M822.9 822.8C750.5 895.2 650.5 940 540 940l200-200 3.7 3.7 79.2 79.1z"
+      style={{
+        fill: "#9ef8c8",
+      }}
+    />
+    <path
+      d="M540 140v800c-110.5 0-210.5-44.8-282.8-117.2S140 650.5 140 540s44.8-210.5 117.2-282.8C329.5 184.8 429.5 140 540 140z"
+      style={{
+        fill: "#4171ca",
+      }}
+    />
+    <path
+      style={{
+        fill: "#5ed8c5",
+      }}
+      d="M740 340 540 540V140z"
+    />
+    <path
+      style={{
+        fill: "#6bcee9",
+      }}
+      d="M940 540H540l200-200z"
+    />
+    <path
+      style={{
+        fill: "#7ee0fb",
+      }}
+      d="M940 540 740 740 540 540z"
+    />
+    <path
+      style={{
+        fill: "#75fbb3",
+      }}
+      d="M740 740 540 940V540z"
+    />
+  </svg>
+);
+
+export default FrontsideMega;
