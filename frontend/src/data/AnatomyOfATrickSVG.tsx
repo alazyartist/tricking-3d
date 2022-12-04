b98b559fd0fd10ea983121118297615fd4cb5a25
Dylan James <alazyartist@gmail.com>
2022-12-04 04:23:29 -0700

    anatomy

---
+++ frontend/src/data/AnatomyOfATrickSVG.tsx
@@ -0,0 +1,593 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const AOAT = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1080" {...props}>
+    <g data-name="Layer 1">
+      <text
+        transform="translate(733.41 140.82)"
+        fontSize={53.7}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-MdIt, Helvetica Neue LT Std"
+        fontWeight={500}
+        fontStyle="italic"
+      >
+        {"Anatomy of a "}
+        <tspan x={345.18} y={0} letterSpacing="-.09em">
+          {"T"}
+        </tspan>
+        <tspan x={372.09} y={0}>
+          {"rick"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(1188.53 220.45)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Landing"}
+      </text>
+      <text
+        transform="translate(613.49 229.4)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        <tspan letterSpacing="-.11em">{"T"}</tspan>
+        <tspan x={12.43} y={0}>
+          {"akeo"}
+        </tspan>
+        <tspan x={70.61} y={0} letterSpacing="-.02em">
+          {"f"}
+        </tspan>
+        <tspan x={78.08} y={0}>
+          {"f"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(912.94 189.13)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Co"}
+        <tspan x={34.8} y={0} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={43.25} y={0} letterSpacing="0em">
+          {"e"}
+        </tspan>
+        <tspan x={57.67} y={0} letterSpacing="-.09em">
+          {"T"}
+        </tspan>
+        <tspan x={70.61} y={0}>
+          {"rick"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(881.12 643.35)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"NEW Co"}
+        <tspan x={102.92} y={0} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={111.37} y={0}>
+          {"e"}
+        </tspan>
+        <tspan x={125.79} y={0} letterSpacing="-.09em">
+          {"T"}
+        </tspan>
+        <tspan x={138.73} y={0}>
+          {"rick"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(843.81 299.22)"
+        fontSize={17.9}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Beginning"}
+      </text>
+      <text
+        transform="translate(741.03 299.22)"
+        fontSize={17.9}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Entrance"}
+      </text>
+      <text
+        transform="translate(944.92 299.22)"
+        fontSize={17.9}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Middle"}
+      </text>
+      <text
+        transform="translate(1037.93 299.22)"
+        fontSize={17.9}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"End"}
+      </text>
+      <text
+        transform="translate(1125.88 299.22)"
+        fontSize={17.9}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Exit"}
+      </text>
+      <text
+        transform="translate(894.77 204.79)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Set of Movements"}
+        <tspan x={0} y={18.79}>
+          {"That meets the criteria"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(909.15 464.34)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Can Be applied to "}
+        <tspan x={-8.68} y={18.79}>
+          {"any of the \u201Celements\u201D"}
+        </tspan>
+        <tspan x={-13.92} y={37.59}>
+          {"of the "}
+        </tspan>
+        <tspan x={28.73} y={37.59} letterSpacing="-.09em">
+          {"T"}
+        </tspan>
+        <tspan x={36} y={37.59}>
+          {"rick. Explains in"}
+        </tspan>
+        <tspan x={-29.75} y={56.39}>
+          {"what way they a"}
+        </tspan>
+        <tspan x={79.64} y={56.39} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={84.29} y={56.39}>
+          {"e changed"}
+        </tspan>
+        <tspan x={-37.43} y={75.18}>
+          {"f"}
+        </tspan>
+        <tspan x={-33.37} y={75.18} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={-28.72} y={75.18}>
+          {"om the element they change"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(613.49 249.54)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Denotes how"}
+        <tspan x={0} y={18.79}>
+          {"many feet, in "}
+        </tspan>
+        <tspan x={0} y={37.59}>
+          {"what o"}
+        </tspan>
+        <tspan x={45.84} y={37.59} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={50.5} y={37.59}>
+          {"de"}
+        </tspan>
+        <tspan x={67.61} y={37.59} letterSpacing="-.09em">
+          {"r"}
+        </tspan>
+        <tspan x={71.11} y={37.59}>
+          {","}
+        </tspan>
+        <tspan x={0} y={56.39}>
+          {"and in which wa"}
+        </tspan>
+        <tspan x={109.09} y={56.39} letterSpacing="-.09em">
+          {"y"}
+        </tspan>
+        <tspan x={114.9} y={56.39} letterSpacing="0em">
+          {"."}
+        </tspan>
+      </text>
+      <text
+        transform="translate(1188.53 240.59)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Denotes how"}
+        <tspan x={0} y={18.79}>
+          {"many feet, in "}
+        </tspan>
+        <tspan x={0} y={37.59}>
+          {"what o"}
+        </tspan>
+        <tspan x={45.84} y={37.59} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={50.5} y={37.59}>
+          {"de"}
+        </tspan>
+        <tspan x={67.61} y={37.59} letterSpacing="-.09em">
+          {"r"}
+        </tspan>
+        <tspan x={71.11} y={37.59}>
+          {","}
+        </tspan>
+        <tspan x={0} y={56.39}>
+          {"and in which wa"}
+        </tspan>
+        <tspan x={109.09} y={56.39} letterSpacing="-.09em">
+          {"y"}
+        </tspan>
+        <tspan x={114.9} y={56.39} letterSpacing="0em">
+          {"."}
+        </tspan>
+      </text>
+      <text
+        transform="rotate(.02 -714174.582 1084103.522)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Denotes orientation"}
+        <tspan x={0} y={18.79}>
+          {"to Di"}
+        </tspan>
+        <tspan x={31.32} y={18.79} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={35.98} y={18.79}>
+          {"ection of Momentum"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(1342.92 240.59)"
+        fontSize={15.66}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Lt, Helvetica Neue LT Std"
+      >
+        {"Denotes orientation"}
+        <tspan x={0} y={18.79}>
+          {"to Di"}
+        </tspan>
+        <tspan x={31.33} y={18.79} letterSpacing="-.02em">
+          {"r"}
+        </tspan>
+        <tspan x={35.98} y={18.79}>
+          {"ection of Movement"}
+        </tspan>
+      </text>
+      <text
+        transform="rotate(.02 -656479.962 1071316.493)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        <tspan letterSpacing="-.11em">{"T"}</tspan>
+        <tspan x={12.43} y={0}>
+          {"akeo"}
+        </tspan>
+        <tspan x={70.61} y={0} letterSpacing="-.02em">
+          {"f"}
+        </tspan>
+        <tspan x={78.08} y={0} letterSpacing="0em">
+          {"f Stance"}
+        </tspan>
+      </text>
+      <text
+        transform="translate(1342.92 220.45)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        {"Landing Stance"}
+      </text>
+      <path
+        fill="none"
+        stroke="#67308f"
+        strokeMiterlimit={10}
+        d="m885.37 274.9 35.8-35.8"
+      />
+      <text
+        transform="translate(913.7 439.73)"
+        fontSize={26.85}
+        fill="#010101"
+        fontFamily="HelveticaNeueLTStd-Roman, Helvetica Neue LT Std"
+      >
+        <tspan letterSpacing="-.05em">{"V"}</tspan>
+        <tspan x={14.93} y={0}>
+          {"ariations"}
+        </tspan>
+      </text>
+      <path
+        fill="none"
+        stroke="#67308f"
+        strokeMiterlimit={10}
+        d="m1445.97 296.16-389.33 140.96M1213.96 314.06l-143.89 93.97M1132.72 314.06l-86.57 82.79M463.7 311.82l420.66 114.11M669.55 331.96l225.99 67.12M783.67 320.77l120.82 62.65M1056.64 311.82l-35.1 82.79M972.11 310.7v80.55M884.36 318.53l55.93 67.13M1128.25 271.55l-50.35-50.35M864.04 222.14l-54.74 54.75M1019.69 242.46l33.56 33.56M969.87 239.1v38.04"
+      />
+      <path
+        fill="none"
+        stroke="#67308f"
+        strokeMiterlimit={10}
+        strokeWidth={4}
+        d="M972.11 550.12v51.16"
+      />
+      <path fill="#67308f" d="m964.13 598.95 7.98 13.82 7.98-13.82h-15.96z" />
+      <path
+        d="M1479.23 1074.81c-2.06-.12-10.61-.12-47.6-.12h-12.34l-74.85-74a3.49 3.49 0 0 1-1-2.21h210.93a3.54 3.54 0 0 1-1 2.22Z"
+        fill="#fff"
+      />
+      <path
+        d="M1553.78 999a3.05 3.05 0 0 1-.78 1.36l-73.91 73.91h-59.59l-74.7-73.88a3 3 0 0 1-.8-1.39h209.76m1.09-1h-211.92a4 4 0 0 0 1.16 3.07l75 74.16H1479.52l74.19-74.2a4 4 0 0 0 1.17-3.07Z"
+        fill="#010101"
+      />
+      <text
+        transform="translate(1399.44 1050.21)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"B"}
+      </text>
+      <text
+        transform="translate(1442.95 1066.88)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"M"}
+      </text>
+      <text
+        transform="translate(1486.37 1052.99)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"E"}
+      </text>
+      <text
+        transform="translate(1357.23 1011.33)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"En"}
+      </text>
+      <text
+        transform="translate(1507.89 1011.33)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"Ex"}
+      </text>
+      <text
+        transform="translate(1412.27 1027.15)"
+        fontSize={16.71}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        <tspan letterSpacing="-.05em">{"T"}</tspan>
+        <tspan x={7.55} y={0} letterSpacing="-.01em">
+          {"r"}
+        </tspan>
+        <tspan x={12.93} y={0}>
+          {"ansition"}
+        </tspan>
+      </text>
+      <path
+        d="M678.23 880.84c-1.78-.1-8.43-.11-34.31-.13h-61.07c-21.94 0-24.73 0-25.16.05h.09l-149.61-1.86a7.52 7.52 0 0 1-2.05-5.28l422.46-146a7.5 7.5 0 0 1-2.19 5.09Z"
+        fill="#fff"
+      />
+      <path
+        d="M828 728.31a6.93 6.93 0 0 1-2 4l-148 148H582.77c-19.82 0-24 0-25.06.05l-149.4-1.86a7.06 7.06 0 0 1-1.77-4.42L828 728.31m1.05-1.42L405.63 873.2a8 8 0 0 0 2.33 6.14l149.81 1.86h25.08c35.37 0 95.62.06 95.65.11L826.75 733a8 8 0 0 0 2.32-6.14Z"
+        fill="#010101"
+      />
+      <text
+        transform="translate(548.26 869.08)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"B"}
+      </text>
+      <text
+        transform="translate(610.16 870.59)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"M"}
+      </text>
+      <text
+        transform="translate(667.52 854.1)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"E"}
+      </text>
+      <text
+        transform="translate(489.06 872.21)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"En"}
+      </text>
+      <text
+        transform="translate(717.75 800.48)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"Ex"}
+      </text>
+      <text
+        transform="translate(593.75 836.82)"
+        fontSize={33.38}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        <tspan letterSpacing=".01em">{"S"}</tspan>
+        <tspan x={16.66} y={0}>
+          {"etup"}
+        </tspan>
+      </text>
+      <path
+        d="M1486.31 1075.8a7 7 0 0 1 2-4.88l138.36-138.32c1.72.1 8 .1 32 .13h80.26l139.74 138.21a7 7 0 0 1 2 4.87Z"
+        fill="#fff"
+      />
+      <path
+        d="M1626.81 933.16h111.91l139.59 138.06a6.47 6.47 0 0 1 1.86 4h-393.34a6.52 6.52 0 0 1 1.86-4l138.12-138.12m-.42-1L1488 1070.57a7.45 7.45 0 0 0-2.17 5.73h395.34a7.52 7.52 0 0 0-2.17-5.73l-139.89-138.34h-23.41c-33 0-89.27 0-89.31-.1Z"
+        fill="#010101"
+      />
+      <text
+        transform="translate(1597.85 985.97)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"B"}
+      </text>
+      <text
+        transform="translate(1669.77 958.79)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"M"}
+      </text>
+      <text
+        transform="translate(1749.65 988.56)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"E"}
+      </text>
+      <text
+        transform="translate(1539.85 1066.26)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"En"}
+      </text>
+      <text
+        transform="translate(1816.99 1066.26)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"Ex"}
+      </text>
+      <text
+        transform="translate(1653.21 1025.83)"
+        fontSize={31.17}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        <tspan letterSpacing="-.04em">{"T"}</tspan>
+        <tspan x={14.09} y={0} letterSpacing="0em">
+          {"r"}
+        </tspan>
+        <tspan x={24.4} y={0}>
+          {"ick"}
+        </tspan>
+      </text>
+      <path
+        d="M681.85 882.53a10.55 10.55 0 0 1 3.06-7.58L888.5 671.37c2 .13 10.57.13 47.25.17h118.11L1259.54 875a10.51 10.51 0 0 1 3.06 7.57Z"
+        fill="#fff"
+      />
+      <path
+        d="M888.64 671.93h130.99c25.47 0 32.16 0 34-.05l205.53 203.28a10 10 0 0 1 2.91 6.72H682.36a10 10 0 0 1 2.91-6.72l203.37-203.23m-.42-1L684.56 874.6a11 11 0 0 0-3.19 8.43h581.71a11 11 0 0 0-3.19-8.43L1054.07 671h-34.44c-48.58 0-131.37-.07-131.41-.14Z"
+        fill="#010101"
+      />
+      <text
+        transform="translate(859.56 780.6)"
+        fontSize={45.86}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"B"}
+      </text>
+      <text
+        transform="translate(1092.78 795.69)"
+        fontSize={45.86}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"E"}
+      </text>
+      <text
+        transform="translate(760.87 868.26)"
+        fontSize={45.86}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"En"}
+      </text>
+      <text
+        transform="translate(1168.67 868.26)"
+        fontSize={45.86}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        {"Ex"}
+      </text>
+      <text
+        transform="translate(927.67 808.77)"
+        fontSize={45.86}
+        fill="#010101"
+        fontFamily="MyriadPro-Regular, Myriad Pro"
+      >
+        <tspan letterSpacing="-.04em">{"T"}</tspan>
+        <tspan x={20.73} y={0} letterSpacing="0em">
+          {"r"}
+        </tspan>
+        <tspan x={35.91} y={0}>
+          {"ick"}
+        </tspan>
+      </text>
+      <path
+        fill="none"
+        stroke="#67308f"
+        strokeMiterlimit={10}
+        d="M832.59 726.89v154.29M927.62 674.04v209.85M1069.58 688.71v193.15M1156.8 774.33v107.53"
+      />
+    </g>
+    <path
+      d="M331.67 866.2s306-71.73 430.73-165.07c194.53-145.52 205.13-207.73 340.75-56.06 129.21 144.5 182.25 242.55 258.74 240.58"
+      fill="none"
+      stroke="#010101"
+      strokeLinecap="round"
+      strokeMiterlimit={10}
+      strokeWidth={12}
+      data-name="Layer 5"
+    />
+  </svg>
+);
+
+export default AOAT;
---
+++ frontend/src/pages/theory/anatomy/index.tsx
@@ -0,0 +1,8 @@
+import AnatomyOfATrick from "@old_pages/theory/anatomy/AnatomyOfATrick";
+import React from "react";
+
+const AnatomyPage = () => {
+  return <AnatomyOfATrick />;
+};
+
+export default AnatomyPage;
