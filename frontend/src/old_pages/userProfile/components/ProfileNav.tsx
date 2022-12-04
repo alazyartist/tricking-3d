3cceb7dddbc5569dbc8bcd68f0c1e34385d611b2
Dylan James <alazyartist@gmail.com>
2022-12-03 20:20:12 -0700

    still switching to typescript

---
+++ frontend/src/old_pages/userProfile/components/ProfileNav.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProfileNav = ({ activeView, setActiveView }) => {
+  return (
+    <div className={"flex gap-4"}>
+      <div
+        className={`${
+          activeView === "Stats" ? "text-teal-400" : ""
+        } mb-2 w-fit rounded-md bg-zinc-900 p-1 px-4`}
+        onClick={() => setActiveView("Stats")}
+      >
+        Overall Stats
+      </div>
+      <div
+        className={`${
+          activeView === "Sessions" ? "text-teal-400" : ""
+        } mb-2 w-fit rounded-md bg-zinc-900 p-1 px-4`}
+        onClick={() => setActiveView("Sessions")}
+      >
+        Session Stats
+      </div>
+    </div>
+  );
+};
+
+export default ProfileNav;
