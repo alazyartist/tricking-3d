ea7c3d061b4e2a4acc640188b0a68ffd47c418c6
Dylan James <alazyartist@gmail.com>
2023-01-07 00:21:36 -0700

    withTRPC

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,10 +4,13 @@
 import type { AppType } from "next/app";
 import dynamic from "next/dynamic";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import "../autocomplete.css";
 import { useRouter } from "next/router";
 import TheoryTabBar from "@components/layout/TheoryTabBar";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 import { useEffect } from "react";
 const AppBackground = dynamic(
   () => import("../components/layout/AppBackground")
@@ -18,17 +21,10 @@ const UserIcon = dynamic(() => import("../components/layout/UserIcon"), {
 const TabBar = dynamic(() => import("../components/layout/TabBar"), {
   ssr: false,
 });
-// import { trpc } from "../utils/trpc";
 
 const MyApp: AppType<{
-  // session: Session | null
-}> = ({
-  Component,
-  pageProps: {
-    // session,
-    ...pageProps
-  },
-}) => {
+  session: Session | null;
+}> = ({ Component, pageProps: { session, ...pageProps } }) => {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       window.addEventListener("load", () => {
@@ -45,16 +41,16 @@ const MyApp: AppType<{
   const path = router.pathname;
   const queryClient = new QueryClient();
   return (
-    // <SessionProvider session={session}>
-    <QueryClientProvider client={queryClient}>
-      <AppBackground />
-      {!path.includes("/landing") && path !== "/" && <UserIcon />}
-      {path.includes("/theory") ? <TheoryTabBar /> : <TabBar />}
-      <Component {...pageProps} />
-    </QueryClientProvider>
-    // </SessionProvider>
+    <SessionProvider session={session}>
+      <QueryClientProvider client={queryClient}>
+        <AppBackground />
+        {!path.includes("/landing") && path !== "/" && <UserIcon />}
+        {path.includes("/theory") ? <TheoryTabBar /> : <TabBar />}
+        <Component {...pageProps} />
+      </QueryClientProvider>
+      //{" "}
+    </SessionProvider>
   );
 };
 
-export default MyApp;
-// export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(MyApp);
