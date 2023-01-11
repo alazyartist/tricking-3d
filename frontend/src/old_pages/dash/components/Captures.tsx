f1e5c5ef2d8fb2b917f5dd2457b2e91e96cb62de
tohzt <steven.g.french@live.com>
2023-01-11 01:03:47 -0700

    Render both capture lists in separate containers

--- frontend/src/old_pages/dash/components/Captures.tsx
+++ frontend/src/old_pages/dash/components/Captures.tsx
@@ -1,10 +1,10 @@
+//import { UserCard } from "./UserCard";
+//import { useApiCreds } from "../../../hooks/useApiCreds";
 import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
-import useApiCreds from "../../../hooks/useApiCreds";
 import { useUserStore } from "../../../store/userStore";
 import CapturedCard from "./CapturedCard";
-import UserCard from "./UserCard";
 
 const Captures = () => {
   const [captured, setCaptured] = useState<any>();
@@ -22,71 +22,55 @@ const Captures = () => {
   return (
     <div
       id="captureContainer"
-      className="flex flex-col place-items-center font-inter"
+      className="flex flex-col border-white border-3 place-items-center font-inter"
     >
-      <div id="myCaptures" className="w_full">
-      
+
+    {/* Caprured Me */}
+      <div id="myCaptures" className="border-blue-500 border-2 w-full">
         <div className="flex gap-2 font-inter font-bold">
-          <div
-            className={display === "captures" ? "text-zinc-300" : "text-zinc-400"}
-            onClick={() => {
-              setDisplay("captures");
-              queryClient.invalidateQueries(["userInfo"]);
-            }}
-          >
+          <div className="text-zinc-300">
             Your Captures
           </div>
         </div>
 
-        <div id="capturedMe" className="w-full">
+        <div className="w-[80vw] overflow-x-auto">
           <div
-            className={
-              display === "captured me" ? "text-zinc-300" : "text-zinc-400"
-            }
-            onClick={() => {
-              setDisplay("captured me");
-              queryClient.invalidateQueries(["userInfo"]);
-            }}
-          >
-            Captured You
-          </div>
-        </div>
-
-      </div>
-      <div className="w-[80vw] overflow-x-auto">
-        <div
-          className={`flex w-full flex-row ${
-            captured?.length >= 4
+            className={`flex w-full flex-row ${captured?.length >= 4
               ? "place-content-start"
               : "place-content-center"
-          }`}
-        >
-          {!!captured &&
-            display === "captures" &&
-            Object.keys(captured).map((key) => (
-              <div
-                onClick={() => nav.push(`/userProfile/${captured[key].uuid}`)}
-                key={`${captured[key].username}`}
-                className="flex flex-row gap-3"
-              >
-                <CapturedCard
-                  name={
-                    captured[key].first_name + " " + captured[key].last_name
-                  }
-                  src={
-                    captured[key].profilePic
-                      ? `./images/${captured[key].uuid}/${captured[key].profilePic}`
-                      : `./images/noimg.jpeg`
-                  }
-                  username={`${captured[key].username}`}
-                />
-              </div>
-            ))}
+              }`}
+          >
+            {!!captured &&
+              Object.keys(captured).map((key) => (
+                <div
+                  onClick={() => nav.push(`/userProfile/${captured[key].uuid}`)}
+                  key={`${captured[key].username}`}
+                  className="flex flex-row gap-3"
+                >
+                  <CapturedCard
+                    name={
+                      captured[key].first_name + " " + captured[key].last_name
+                    }
+                    src={
+                      captured[key].profilePic
+                        ? `./images/${captured[key].uuid}/${captured[key].profilePic}`
+                        : `./images/noimg.jpeg`
+                    }
+                    username={`${captured[key].username}`}
+                  />
+                </div>
+              ))}
+          </div>
         </div>
+      </div>
 
+      <div id="capturedMe" className="border-red-500 border-2 w-full">
+        {/* Caprured Me */}
+        <div className="text-zinc-300 font-bold">
+          Captured You
+        </div>
         <div className="flex w-[80vw] flex-row place-content-center place-items-start overflow-x-auto">
           {!!capturedYou &&
-            display === "captured me" &&
             Object.keys(capturedYou).map((key) => (
               <div
                 onClick={() =>
@@ -112,6 +96,7 @@ const Captures = () => {
             ))}
         </div>
       </div>
+
     </div>
   );
 };
