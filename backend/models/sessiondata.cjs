71a6a08fbaf5ac6f5ae9b763b060d11fe55eb0d0
Dylan James <alazyartist@gmail.com>
2023-01-25 17:22:16 -0700

    fix some erros

--- backend/models/sessiondata.cjs
+++ backend/models/sessiondata.cjs
@@ -43,6 +43,7 @@ module.exports = (sequelize) => {
 			bail: DataTypes.INTEGER,
 			createdAt: DataTypes.DATE,
 			updatedAt: DataTypes.DATE,
+			totalScore: DataTypes.FLOAT,
 		},
 		{
 			sequelize,
