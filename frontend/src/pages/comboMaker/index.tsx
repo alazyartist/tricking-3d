5bea8c3c9e20354802a16d115a453f59bde2367e
tohzt <steven.g.french@live.com>
2023-02-23 03:19:49 -0700

    got tricks organized, kinda

--- frontend/src/pages/comboMaker/index.tsx
+++ frontend/src/pages/comboMaker/index.tsx
@@ -6,20 +6,48 @@ const ComboMaker = () => {
   // @TODO: Create a component that makes a usable trick object from useGetTricks
   const { data: allTricks } = useGetTricks();
 
+  const [showTricks, setShowTricks] = useState(true);
+  const [showTransitions, setShowTransitions] = useState(true);
+  const [showStances, setShowStances] = useState(true);
+
 
   return (
     <div className="screen flex-col justify-start">
-      <div className="w-screen flex-center bg-white">
-        <button className="btn-trick-type">Trick</button>
-        <button className="btn-trick-type">Transition</button>
-        <button className="btn-trick-type">Stance</button>
+      <div className="w-screen flex-center bg-white mb-1">
+        <button
+          className={"btn-trick-type" + (showTricks ? " bg-green-400" : " bg-red-400")}
+          onClick={() => setShowTricks(!showTricks)}
+        >Trick</button>
+        <button
+          className={"btn-trick-type" + (showTransitions ? " bg-green-400" : " bg-red-400")}
+          onClick={() => setShowTransitions(!showTransitions)}
+        >Transition</button>
+        <button
+          className={"btn-trick-type" + (showStances ? " bg-green-400" : " bg-red-400")}
+          onClick={() => setShowStances(!showStances)}
+        >Stance</button>
       </div>
-      <div className="h-full flex flex-1 overflow-auto bg-red-300">
-        <div className="flex flex-row flex-wrap gap-4">
+      <div className="pt-2 h-full w-full flex flex-1 overflow-auto">
+        <div className="flex flex-row justify-center flex-wrap gap-4">
+
           {allTricks && allTricks.map((trick) => {
-            if (trick.type !== "Trick") {
-              console.log("trick: ", trick);
+            console.log("trick var ", trick.Variations);
+            let variations: String[] = [];
+
+            {
+              trick.Variations && trick.Variations.map((variation) => {
+                console.log("var ", variation);
+
+                if (variation.Variation) {
+                    console.log("varName ", variation.Variation);
+                    variations.push(variation.Variation.name);
+                }
+              })
             }
+
+            if (!showTricks && trick.type === "Trick") return null;
+            if (!showTransitions && trick.type === "Transition") return null;
+            if (!showStances && trick.type === "Stance") return null;
             return (
               <Trick
                 startingStance={trick.takeoffStance}
@@ -27,9 +55,10 @@ const ComboMaker = () => {
                 type={trick.type}
                 name={trick.name}
                 id={trick.trick_id}
-                variations="" />
+                variations={variations} />
             )
-          })}
+          })
+          }
         </div>
       </div>
     </div>
--- frontend/src/pages/comboMaker/trick/index.tsx
+++ frontend/src/pages/comboMaker/trick/index.tsx
@@ -8,16 +8,22 @@ interface TrickProp {
   id: String;
   variations?: Array<String>;
 }
-const Trick: React.FC = (trick) => {
+const Trick: { (trick: TrickProp): JSX.Element; } = (trick) => {
 
   return (
-    <div className="w-[40vw] flex-col-center bg-red-600 border-2 border-white p-2">
-    <p className="bg-zinc-800 w-full text-zinc-600 flex-center">{trick.type}</p>
+    <div className="w-[40vw] flex-col-center bg-zinc-600 border-2 border-white p-2 rounded-lg">
+      <p className="bg-zinc-800 w-full text-zinc-600 flex-center">{trick.type}</p>
       <p className="bg-zinc-800 w-full text-zinc-600 flex-center">{trick.name}</p>
       <p className="bg-zinc-500 w-full text-zinc-900 flex-center">{trick.startingStance}</p>
       <p className="bg-zinc-600 w-full text-zinc-800 flex-center">{trick.landingStance}</p>
-      <p className="bg-zinc-700 w-full text-black flex-center">{trick.variations}</p>
+      {trick.variations &&
+        trick.variations.map((variation) => (
+          <p className="bg-blue-700 w-full text-black flex-center">{variation}</p>
+        ))
+      }
+      {/*
       <p className="bg-zinc-900 w-full text-zinc-500 flex-center">{trick.id}</p>
+      */}
     </div>
   )
 }
