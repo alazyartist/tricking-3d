d699afbceea5afc97393036f56794c464417fac9
Dylan James <alazyartist@gmail.com>
2023-02-26 00:55:22 -0700

    testing

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   // Assume a "Cookie:nextjs=fast" header to be present on the incoming request
   // Getting cookies from the request using the `RequestCookies` API
-  const cookie = request.cookies.get("jwt")?.value;
-  // console.log(cookie); // => 'fast'
-
+  // const cookie = request.cookies.get("jwt")?.value;
+  // console.log("cooki", cookie); // => 'fast'
+  // // let secret = new TextEncoder().encode(process.env.ACCESS_TOKEN_SECRET);
+  // // let decoded = await jwtVerify(cookie, secret, { algorithms: ["HS256"] });
+  // console.log(cookie ? "loggedIn" : "NotLoggedIn");
   //   // Setting cookies on the response using the `ResponseCookies` API
   //   const response = NextResponse.next()
   //   response.cookies.set('vercel', 'fast')
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -2,8 +2,13 @@ import Home from "@old_pages/home/Home";
 import { NextPage } from "next";
 import React from "react";
 
-const HomePage: NextPage = () => {
+const HomePage: NextPage = (props) => {
+  console.log(props);
   return <Home />;
 };
 
 export default HomePage;
+
+// export async function getStaticProps() {
+//   return { props: { test: "HelloDylan" } };
+// }
