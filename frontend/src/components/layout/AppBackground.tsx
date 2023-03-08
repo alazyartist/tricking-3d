4289fcc94558427afda9cdb3597415709fdda34c
Dylan James <alazyartist@gmail.com>
2023-03-07 22:39:45 -0700

    moving background

--- frontend/src/components/layout/AppBackground.tsx
+++ frontend/src/components/layout/AppBackground.tsx
@@ -1,4 +1,5 @@
 import BackgroundCircles from "@admin/components/BackgroundCircles";
+import MovingBackground from "@old_pages/landing/components/MovingBackground";
 import React from "react";
 
 function AppBackground(props) {
@@ -7,7 +8,8 @@ function AppBackground(props) {
       id="App-Background"
       className="fixed top-0 left-0 z-[-2] h-screen w-screen place-content-center bg-gradient-to-b from-zinc-900 to-zinc-800"
     >
-      <BackgroundCircles />
+      <MovingBackground />
+      {/* <BackgroundCircles /> */}
       {props.children}
     </div>
   );
