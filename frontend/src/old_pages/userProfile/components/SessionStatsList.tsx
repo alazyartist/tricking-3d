ef9fcac5ca493dd0b51d07a4316a91b8b44e7948
Dylan James <alazyartist@gmail.com>
2023-03-08 20:40:41 -0700

    extract into component

--- frontend/src/old_pages/userProfile/components/SessionStatsList.tsx
+++ frontend/src/old_pages/userProfile/components/SessionStatsList.tsx
@@ -13,33 +13,12 @@ const SessionStatsList = ({
     <div className="no-scrollbar h-full w-full overflow-hidden overflow-y-scroll">
       <ProfileNav setActiveView={setActiveView} activeView={activeView} />
       <div className="flex h-[100%] flex-col place-content-start gap-1">
-        {profileInfo.SessionSummaries.map((summary) => {
-          let totalPoints = summary.SessionData.map(
-            (data) => data.totalScore
-          ).reduce((sum, b) => sum + b, 0);
-          console.log(summary.SessionData);
-          return (
-            <div
-              className="flex flex-col justify-between gap-2 rounded-md bg-zinc-900 p-2"
-              onClick={() => {
-                setActiveSummary(summary);
-              }}
-              key={Math.random() + "link"}
-            >
-              <div className="flex justify-between gap-2">
-                <div className="px-2">{summary.name}</div>
-                <div className="px-2 font-bold">{totalPoints.toFixed(2)}</div>
-              </div>
-              <div className="h-[30px] w-full">
-                <PowerAverageComboLineChart
-                  data={summary.SessionData.sort((a, b) =>
-                    a.clipStart > b.clipStart ? -1 : 1
-                  )}
-                />
-              </div>
-            </div>
-          );
-        })}
+        {profileInfo.SessionSummaries.map((summary) => (
+          <SessionSummaryCard
+            setActiveSummary={setActiveSummary}
+            summary={summary}
+          />
+        ))}
         <div className="p-2 font-bold text-zinc-300 drop-shadow-md">
           no more sessions...
         </div>
@@ -49,3 +28,39 @@ const SessionStatsList = ({
 };
 
 export default SessionStatsList;
+
+interface SSCProps {
+  summary: any;
+  setActiveSummary?: any;
+}
+export const SessionSummaryCard: React.FC<SSCProps> = ({
+  summary,
+  setActiveSummary,
+}) => {
+  let totalPoints = summary?.SessionData?.map((data) => data.totalScore).reduce(
+    (sum, b) => sum + b,
+    0
+  );
+  console.log("summarySD", summary.SessionData);
+  return (
+    <div
+      className="flex flex-col justify-between gap-2 rounded-md bg-zinc-900 p-2"
+      onClick={() => {
+        setActiveSummary(summary);
+      }}
+      key={Math.random() + "link"}
+    >
+      <div className="flex justify-between gap-2">
+        <div className="px-2">{summary.name}</div>
+        <div className="px-2 font-bold">{totalPoints?.toFixed(2)}</div>
+      </div>
+      <div className="h-[30px] w-full">
+        <PowerAverageComboLineChart
+          data={summary.SessionData?.sort((a, b) =>
+            a.clipStart > b.clipStart ? -1 : 1
+          )}
+        />
+      </div>
+    </div>
+  );
+};
