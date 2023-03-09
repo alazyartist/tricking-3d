5b8640c2a6cb56e1e52bdcc59bc164b22b1b9e26
Dylan James <alazyartist@gmail.com>
2023-03-09 15:10:30 -0700

    grid setup

--- frontend/src/pages/compare/[...sessions]/index.tsx
+++ frontend/src/pages/compare/[...sessions]/index.tsx
@@ -8,10 +8,29 @@ const CompareSessions = () => {
   return (
     <div className="text-zinc-300">
       CompareSessions
-      <div>
-        {Array.isArray(sessions)
-          ? sessions.map((s) => <div className="p-2">{s}</div>)
-          : sessions}
+      <div className="mt-14 grid grid-cols-[1fr_3fr]">
+        <div className="flex h-full w-full place-content-end place-items-center">
+          Info
+        </div>
+        <div className="grid grid-cols-3 place-items-center">
+          {Array.isArray(sessions)
+            ? sessions.map((s) => <div className="p-2">{s}</div>)
+            : sessions}
+        </div>
+        <div className="flex h-full w-full flex-col place-content-center place-items-end">
+          {["most used", "favorite", "total tricks"].map((s) => (
+            <div>{s}</div>
+          ))}
+        </div>
+        <div className="grid grid-cols-3 place-items-center">
+          {[1, 2, 3].map((s) => (
+            <div>
+              <div>{s}</div>
+              <div>{s}</div>
+              <div>{s}</div>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
