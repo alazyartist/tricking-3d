47572eb8e0af7522e45636019214d3cd9c44e7ba
Dylan James <alazyartist@gmail.com>
2023-03-12 04:17:43 -0600

    background change

--- frontend/src/pages/debate/index.tsx
+++ frontend/src/pages/debate/index.tsx
@@ -6,7 +6,7 @@ const DebatePage = () => {
 
   return (
     <>
-      <div className="backrop-blur-xl mt-14 flex h-[80vh] w-full flex-col place-items-center gap-2 overflow-hidden overflow-y-scroll bg-zinc-900 bg-opacity-70 p-4 font-inter">
+      <div className="backrop-blur-xl flex h-[80vh] w-full flex-col place-items-center gap-2 overflow-hidden overflow-y-scroll bg-zinc-900 bg-opacity-70 p-4 pt-14 font-inter">
         <div className=" h-fit w-full rounded-md bg-zinc-100 bg-opacity-40 p-2 text-xl text-zinc-100">
           Proposition Here
           <p className="text-xs">
@@ -29,12 +29,12 @@ const DebatePage = () => {
           </div>
         </div>
         <div className="mb-4 grid h-full w-full grid-cols-2 gap-2">
-          <div className="flex h-full w-full flex-col gap-2 rounded-md bg-emerald-300 bg-opacity-40 p-2">
+          <div className="flex h-full w-full flex-col gap-2 rounded-md border-[1px] border-emerald-300 bg-opacity-40 p-2">
             {messagesYay.map((m) => (
               <MessageDisplay side={"left"} />
             ))}
           </div>
-          <div className="flex h-full w-full flex-col gap-2 rounded-md bg-red-300 bg-opacity-40 p-2 pt-12">
+          <div className="flex h-full w-full flex-col gap-2 rounded-md border-[1px] border-red-300 bg-opacity-40 p-2 pt-12">
             {messagesNay.map((m) => (
               <MessageDisplay side={"right"} />
             ))}
@@ -59,7 +59,7 @@ const MessageDisplay = ({ side }) => {
     <div className="relative flex rounded-md bg-zinc-500">
       {side === "left" && <div>Test message text here</div>}
       <div
-        className={`relative ${side}-1 bottom-1 h-6 w-6 rounded-full bg-indigo-600`}
+        className={`relative ${side}-1 bottom-1 h-6 w-6 flex-shrink-0 rounded-full bg-indigo-600`}
       ></div>
       {side === "right" && <div>Test message text here</div>}
     </div>
