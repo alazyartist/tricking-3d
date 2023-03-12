e5dc3f99279e21c350980b0d78bc528bd9542857
Dylan James <alazyartist@gmail.com>
2023-03-12 04:04:23 -0600

    debate scaffold continues

--- frontend/src/pages/debate/index.tsx
+++ frontend/src/pages/debate/index.tsx
@@ -1,32 +1,78 @@
 import React from "react";
 
 const DebatePage = () => {
+  let messagesYay = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3];
+  let messagesNay = [1, 2, 3, 1, 1, 2, 3];
+
   return (
-    <div className="mt-14 flex h-[80vh] w-full flex-col place-items-center gap-2 p-4">
-      <div className="h-[150px] w-full rounded-md bg-sky-300">test</div>
-      <div className="grid h-full w-full grid-cols-2 gap-2">
-        <div className="flex h-full w-full flex-col gap-2 rounded-md bg-red-300 p-2">
-          {[1, 2, 3].map((m) => (
-            <MessageDisplay />
-          ))}
+    <>
+      <div className="backrop-blur-xl mt-14 flex h-[80vh] w-full flex-col place-items-center gap-2 overflow-hidden overflow-y-scroll bg-zinc-900 bg-opacity-70 p-4 font-inter">
+        <div className=" h-fit w-full rounded-md bg-zinc-100 bg-opacity-40 p-2 text-xl text-zinc-100">
+          Proposition Here
+          <p className="text-xs">
+            This is where the general proposition for the debate will go it will
+            show us a general overfiew of the information to be debated up on
+            and should end with a yes or format.ex. Double-cork.mega should be
+            worth more than Double-full-snapu
+          </p>
         </div>
-        <div className="flex h-full w-full flex-col gap-2 rounded-md bg-red-500 p-2">
-          {[1, 2, 3].map((m) => (
-            <MessageDisplay />
-          ))}
+        <div className={` grid grid-cols-2 gap-2`}>
+          <div
+            className={`text-zinc h-fit bg-opacity-40 text-xl font-bold text-emerald-300`}
+          >
+            Yay
+          </div>
+          <div
+            className={`text-zinc h-fit bg-opacity-40 text-xl font-bold text-red-300`}
+          >
+            Nay
+          </div>
+        </div>
+        <div className="mb-4 grid h-full w-full grid-cols-2 gap-2">
+          <div className="flex h-full w-full flex-col gap-2 rounded-md bg-emerald-300 bg-opacity-40 p-2">
+            {messagesYay.map((m) => (
+              <MessageDisplay side={"left"} />
+            ))}
+          </div>
+          <div className="flex h-full w-full flex-col gap-2 rounded-md bg-red-300 bg-opacity-40 p-2 pt-12">
+            {messagesNay.map((m) => (
+              <MessageDisplay side={"right"} />
+            ))}
+          </div>
+          <div className=" w-full rounded-md bg-emerald-500 p-2">
+            {messagesYay.length}
+          </div>
+          <div className=" w-full rounded-md bg-red-500 p-2">
+            {messagesNay.length}
+          </div>
         </div>
       </div>
-    </div>
+      <MessageInput />
+    </>
   );
 };
 
 export default DebatePage;
 
-const MessageDisplay = () => {
+const MessageDisplay = ({ side }) => {
   return (
-    <div className="flex rounded-md bg-zinc-500">
-      <div className="h-6 w-6 rounded-full bg-indigo-600"></div>
-      <div>Test message text here</div>
+    <div className="relative flex rounded-md bg-zinc-500">
+      {side === "left" && <div>Test message text here</div>}
+      <div
+        className={`relative ${side}-1 bottom-1 h-6 w-6 rounded-full bg-indigo-600`}
+      ></div>
+      {side === "right" && <div>Test message text here</div>}
+    </div>
+  );
+};
+
+const MessageInput = () => {
+  return (
+    <div className="w-full p-4 pb-12">
+      <input
+        className="w-full rounded-md bg-zinc-700 p-2 text-zinc-300"
+        placeholder="contribute to the debate"
+      />
     </div>
   );
 };
