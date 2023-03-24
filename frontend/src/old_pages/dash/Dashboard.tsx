6ac08cda6d491d1cee1f206ef6c12c90436071da
Dylan James <alazyartist@gmail.com>
2023-03-23 23:05:32 -0600

    dashboard changes

--- frontend/src/old_pages/dash/Dashboard.tsx
+++ frontend/src/old_pages/dash/Dashboard.tsx
@@ -14,9 +14,6 @@ function Dashboard() {
   const user = useUserStore((s) => s.user);
   const { profilePic, uuid } = useUserStore((s) => s.userInfo);
 
-  const { data: trickData } = useGetTricks();
-  console.log(trickData);
-
   return (
     <div className="mt-14 flex flex-col place-content-center place-items-center gap-2 text-zinc-400">
       <div className="p-4">
@@ -39,10 +36,9 @@ function Dashboard() {
           p-4 
         "
         >
-          {/* <Captures /> */}
+          <Captures />
         </div>
       </>
-      {trickData && <RadarChart data={trickData} />}
       {/* <UserList /> */}
       <button className="fixed right-5 bottom-14" onClick={() => logout()}>
         Logout
