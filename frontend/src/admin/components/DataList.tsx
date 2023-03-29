851469a91023385ea0caf0a531cada1735a88d1e
Dylan James <alazyartist@gmail.com>
2023-03-28 19:17:06 -0600

    display cliptotals

--- frontend/src/admin/components/DataList.tsx
+++ frontend/src/admin/components/DataList.tsx
@@ -1,3 +1,4 @@
+import { trpc } from "@utils/trpc";
 import React from "react";
 import { FaCheck, FaCircle } from "react-icons/fa";
 import useGetCombos from "../../api/useGetCombos";
@@ -5,21 +6,25 @@ import useGetTricks, { useGetTrickPoints } from "../../api/useGetTricks";
 import DataListCommandBar from "../DataListCommandBar";
 import MakeNewTrickModal from "./sessionreview/MakeNewTrickModal";
 import { useSessionSummariesStore } from "./sessionreview/SessionSummaryStore";
-
+import * as d3 from "d3";
 const DataList = () => {
-  const { data: tricks } = useGetTricks();
-  const { data: combos } = useGetCombos();
-  const { data: trickPoints, refetch } = useGetTrickPoints();
+  // const { data: tricks } = useGetTricks();
+  // const { data: combos } = useGetCombos();
+  const { data: tricks } = trpc.trick.findAllwithComboClips.useQuery();
+  const { data: combos } = trpc.combos.getAll.useQuery();
+  // const { data: trickPoints, refetch } = useGetTrickPoints();
   let trickMakerOpen = useSessionSummariesStore((s) => s.trickMakerOpen);
+  if (!tricks) return <div>No Tricks</div>;
+
   return (
     <div className="no-scrollbar flex max-h-[70vh] w-full flex-col place-items-center gap-2 overflow-y-scroll rounded-xl pb-14">
       <h1
-        onClick={() => refetch()}
+        // onClick={() => refetch()}
         className="sticky top-0 h-full w-full bg-zinc-800 p-2 text-center text-xl font-bold"
       >
         TRICKS, STANCES, &#38; TRANSITIONS
       </h1>
-      <div className="w-[70vw] text-sm">
+      <div className="w-[90vw] text-sm">
         {tricks
           ?.sort((a, b) => {
             if (a.type < b.type) return 1;
@@ -35,34 +40,7 @@ const DataList = () => {
             });
           })
           ?.map((trick) => (
-            <div
-              key={Math.random()}
-              onClick={() => console.log(trick)}
-              className=" grid  w-full grid-cols-6 place-items-center justify-between p-2 odd:bg-zinc-700 odd:bg-opacity-70 even:bg-zinc-900 even:bg-opacity-70"
-            >
-              <div className="col-span-3 max-w-[1/3] place-self-start">
-                {trick?.name}
-              </div>
-              <div className="col-span-1 flex place-items-center text-sm">
-                {trick?.type.slice(0, 6)}
-              </div>
-              <div className="col-span-1">
-                {trick.pointValue}
-                {/* {trickPoints?.map((tp) => {
-									return tp?.name === trick?.name && tp?.Total?.toFixed(2);
-								})}
-								{trick.type === "Stance" && trick.pointValue?.toFixed(2)}
-								{trick.type === "Transition" && trick.pointValue?.toFixed(2)} */}
-              </div>
-              <div className="col-span-1 flex place-content-end place-items-center gap-2">
-                DA
-                {trick?.defaultAnimation ? (
-                  <FaCheck className="text-emerald-500" />
-                ) : (
-                  <FaCircle className="text-red-700" />
-                )}
-              </div>
-            </div>
+            <DLTrickDisplay trick={trick} />
           ))}
       </div>
       <h1 className="sticky top-0 h-full w-full bg-zinc-800 p-2 text-center text-xl font-bold">
@@ -78,9 +56,9 @@ const DataList = () => {
           ?.map((combo) => (
             <div
               key={Math.random()}
-              className="grid w-[70vw] grid-cols-4 place-content-center place-items-center justify-between p-2 text-sm odd:bg-zinc-700 odd:bg-opacity-70 even:bg-zinc-900 even:bg-opacity-70"
+              className="grid w-[90vw] grid-cols-6 place-content-center place-items-center justify-between p-2 text-sm odd:bg-zinc-700 odd:bg-opacity-70 even:bg-zinc-900 even:bg-opacity-70"
             >
-              <div className=" col-span-2 max-w-[120px] place-self-start overflow-hidden whitespace-pre-wrap md:max-w-[400px]">
+              <div className=" col-span-3 max-w-[120px] place-self-start overflow-hidden whitespace-pre-wrap md:max-w-[400px]">
                 {combo?.name}
               </div>
               <div className="text-center">{combo?.pointValue}</div>
@@ -92,6 +70,13 @@ const DataList = () => {
                   <FaCircle className="text-red-700" />
                 )}
               </div>
+              <p
+                style={{
+                  color: d3.interpolateRdYlGn(combo?.Clips?.length / 10),
+                }}
+              >
+                {combo?.Clips?.length}
+              </p>
               {/* <div className='flex place-content-end place-items-center gap-2'>
 							CA
 							{combo?.comboArray ? (
@@ -110,3 +95,47 @@ const DataList = () => {
 };
 
 export default DataList;
+
+const DLTrickDisplay = ({ trick }) => {
+  let numOfClips = trick.combos
+    .map((combo) => combo.Clips.length)
+    .reduce((sum, b) => sum + b, 0);
+
+  //   (sum, b) => sum + b.reduce((sum2, b2) => sum2 + b2.Clips.length, 0),
+  //   0
+  // );
+
+  return (
+    <div
+      key={Math.random()}
+      onClick={() => console.log(trick)}
+      className=" grid  w-full grid-cols-7 place-items-center justify-between p-2 odd:bg-zinc-700 odd:bg-opacity-70 even:bg-zinc-900 even:bg-opacity-70"
+    >
+      <div className="col-span-3 max-w-[1/3] place-self-start">
+        {trick?.name}
+      </div>
+      <div className="col-span-1 flex place-items-center text-sm">
+        {trick?.type?.slice(0, 6)}
+      </div>
+      <div className="col-span-1">
+        {trick.pointValue}
+        {/* {trickPoints?.map((tp) => {
+          return tp?.name === trick?.name && tp?.Total?.toFixed(2);
+        })}
+        {trick.type === "Stance" && trick.pointValue?.toFixed(2)}
+        {trick.type === "Transition" && trick.pointValue?.toFixed(2)} */}
+      </div>
+      <div className="col-span-1 flex place-content-end place-items-center gap-2">
+        DA
+        {trick?.defaultAnimation ? (
+          <FaCheck className="text-emerald-500" />
+        ) : (
+          <FaCircle className="text-red-700" />
+        )}
+      </div>
+      <p style={{ color: d3.interpolateRdYlGn(numOfClips / 10) }}>
+        {numOfClips}
+      </p>
+    </div>
+  );
+};
