680626127d41440093178aa67f0f687e074e8011
Dylan James <alazyartist@gmail.com>
2023-06-17 23:33:32 -0600

    center usercard

--- frontend/src/old_pages/dash/components/UserCard.tsx
+++ frontend/src/old_pages/dash/components/UserCard.tsx
@@ -26,7 +26,7 @@ const UserCard: React.FC<Props> = (props) => {
 
   return (
     <>
-      <div className="mx-2 flex w-[70vw] place-items-center justify-between rounded-xl bg-zinc-800 px-4 sm:w-[50vw] md:w-[40vw] lg:w-[25vw] lg:place-self-start xl:w-[20vw]">
+      <div className="mx-2 flex w-[70vw] place-items-center justify-between rounded-xl bg-zinc-800 px-4 sm:w-[50vw] md:w-[40vw] lg:w-[25vw] xl:w-[20vw]">
         <div className="flex flex-col place-items-center">
           <img src={props.src} className="m-2 h-20 w-20 rounded-full" />
           {/* {props.edit && (
