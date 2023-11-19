186008034bccb6784dc339f2c26a6b69a651a2df
Dylan James <alazyartist@gmail.com>
2023-11-18 19:25:03 -0700

    yeet

--- frontend/src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "../../styles/globals.css";
-const SandboxLayout: React.FC<{ children: React.ReactNode }> = ({
-  children,
-}) => {
-  return <>{children}</>;
-};
-
-export default SandboxLayout;
