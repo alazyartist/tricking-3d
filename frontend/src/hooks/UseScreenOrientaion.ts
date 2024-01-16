9bd4336459c97e8da2ea6be2116faed53f227ec8
Dylan James <alazyartist@gmail.com>
2024-01-15 23:51:30 -0700

    fix useScreenOrientaion bug

--- frontend/src/hooks/UseScreenOrientaion.ts
+++ frontend/src/hooks/UseScreenOrientaion.ts
@@ -1,14 +1,11 @@
 import { useState, useEffect } from "react";
 
 const useScreenOrientation = () => {
-  const [orientation, setOrientation] = useState(
-    window?.innerWidth > window?.innerHeight
-      ? "landscape"
-      : "portrait" ?? "portrait"
-  );
+  const [orientation, setOrientation] = useState("portrait");
 
   useEffect(() => {
     if (typeof window === "undefined") setOrientation("portrait");
+
     const handleResize = () => {
       setOrientation(
         window.innerWidth > window.innerHeight ? "landscape" : "portrait"
@@ -17,6 +14,11 @@ const useScreenOrientation = () => {
     window.addEventListener("orientationchange", handleResize);
 
     window.addEventListener("resize", handleResize);
+    if (typeof window !== "undefined") {
+      setOrientation(
+        window.innerWidth > window.innerHeight ? "landscape" : "portrait"
+      );
+    }
     return () => {
       window.removeEventListener("resize", handleResize);
       window.removeEventListener("orientationchange", handleResize);
