6f415110b29d53bc29f89d78757b30d148ff7a0a
Dylan James <alazyartist@gmail.com>
2024-01-30 18:18:08 -0700

    link trickGrpah

--- frontend/src/pages/experiments/index.tsx
+++ frontend/src/pages/experiments/index.tsx
@@ -33,6 +33,14 @@ const ExperimentsPage = () => {
           Debates
         </div>
       </Link>
+      <Link
+        href={"/trickgraph"}
+        className=" mb-2 mt-1 w-[70vw] max-w-[600px] rounded-xl bg-zinc-800 bg-opacity-80 p-2 text-center font-inter text-xl text-zinc-300 shadow-[0_0_8px_1px_rgba(0,0,0,0.3)] "
+      >
+        <div className="flex place-content-center place-items-center gap-2">
+          Trick Graph
+        </div>
+      </Link>
     </div>
   );
 };
