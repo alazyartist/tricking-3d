5dcb67efcc42c49306991745b5a5657e8f257ec0
Dylan James <alazyartist@gmail.com>
2024-01-31 20:50:30 -0700

    termsofservice

--- frontend/src/pages/policies/termsofservice.tsx
+++ frontend/src/pages/policies/termsofservice.tsx
@@ -5,7 +5,7 @@ const TermsOfService = () => {
     <div className="relative left-[5vw] top-[5vh] mb-[14vh] flex h-full w-[90vw] flex-col rounded-xl bg-zinc-800 bg-opacity-70 p-4 text-zinc-300 md:left-[20vw] md:mb-0 md:w-[60vw]">
       <h1 className="mb-4 text-3xl font-bold">Trickedex Terms of Service</h1>
 
-      <h2 className="mb-2 text-2xl font-bold">1. Introduction</h2>
+      <h2 className="mb-2 text-xl font-bold">1. Introduction</h2>
       <p className="mb-4">
         Welcome to Trickedex. These Terms of Service ("Terms") govern your use
         of the Trickedex website, mobile application, and services
@@ -13,14 +13,14 @@ const TermsOfService = () => {
         agree to be bound by these Terms.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">2. Privacy Policy</h2>
+      <h2 className="mb-2 text-xl font-bold">2. Privacy Policy</h2>
       <p className="mb-4">
         Our Privacy Policy describes how we handle the information you provide
         to us when you use our Services. You understand that through your use of
         the Services, you consent to the collection and use of this information.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">3. Service Description</h2>
+      <h2 className="mb-2 text-xl font-bold">3. Service Description</h2>
       <p className="mb-4">
         Trickedex is a dynamic digital platform designed for enthusiasts and
         practitioners of Tricking. It functions as an extensive directory,
@@ -33,7 +33,7 @@ const TermsOfService = () => {
         resource for both beginners and experienced trickers alike.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">4. User Accounts</h2>
+      <h2 className="mb-2 text-xl font-bold">4. User Accounts</h2>
       <p className="mb-4">
         Account Creation: To access most features of the Service, you must
         create an account.
@@ -44,7 +44,7 @@ const TermsOfService = () => {
         use of your account.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">5. User Content</h2>
+      <h2 className="mb-2 text-xl font-bold">5. User Content</h2>
       <p className="mb-4">
         Content Ownership: Users retain ownership of the content they submit to
         the Service.
@@ -55,7 +55,7 @@ const TermsOfService = () => {
         content.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">6. Acceptable Use Policy</h2>
+      <h2 className="mb-2 text-xl font-bold">6. Acceptable Use Policy</h2>
       <p className="mb-4">
         You agree not to misuse the Service or help anyone else do so.
         Prohibited activities include (but are not limited to) harassing,
@@ -63,7 +63,7 @@ const TermsOfService = () => {
         rights, and engaging in fraudulent or illegal activities.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">
+      <h2 className="mb-2 text-xl font-bold">
         7. Service Modifications and Termination
       </h2>
       <p className="mb-4">
@@ -72,7 +72,7 @@ const TermsOfService = () => {
         Accounts may be terminated for violations of these Terms.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">
+      <h2 className="mb-2 text-xl font-bold">
         8. Disclaimers and Limitations of Liability
       </h2>
       <p className="mb-4">
@@ -81,7 +81,7 @@ const TermsOfService = () => {
         error-free. Liability is limited to the extent permitted by law.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">9. Indemnification</h2>
+      <h2 className="mb-2 text-xl font-bold">9. Indemnification</h2>
       <p className="mb-4">
         You agree to indemnify and hold harmless Trickedex and its officers,
         directors, employees, and agents from any claims, damages, losses,
@@ -89,19 +89,19 @@ const TermsOfService = () => {
         your use of the Service.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">10. Governing Law</h2>
+      <h2 className="mb-2 text-xl font-bold">10. Governing Law</h2>
       <p className="mb-4">
         These Terms shall be governed by the laws of the United States.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">11. Changes to Terms</h2>
+      <h2 className="mb-2 text-xl font-bold">11. Changes to Terms</h2>
       <p className="mb-4">
         Trickedex reserves the right to modify these Terms at any time.
         Continued use of the Service after changes indicates acceptance of the
         new Terms.
       </p>
 
-      <h2 className="mb-2 text-2xl font-bold">12. Contact Information</h2>
+      <h2 className="mb-2 text-xl font-bold">12. Contact Information</h2>
       <p className="mb-4">
         For any questions about these Terms, please contact us at [contact
         information].
